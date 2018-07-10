e864c18d39a41fc346a5cec7fb93093d59b6affa
David Upton <davidrkupton@users.noreply.github.com>
2018-07-10 09:39:35 -0400

    Boston theme (#1)
    * Adds initial boston theme to repo.
    
    * Adds adminimal theme to repo.
    
    * Set bos_theme as starting theme for the site.
    
    * Updates node to v8.9.
    
    * Modifies modules downloaded and enabled on install.
    
    * Improvements to Phing install scripts.

--- docroot/sites/default/settings/default.local.settings.php
+++ docroot/sites/default/settings/default.local.settings.php
@@ -8,11 +8,26 @@
    * BOSTON.GOV NOTE: Database array is set by Phing script in setup.xml
    *    setup:drupal:local_settings_php:write
   */
+
+  // make the default config storage for import/export ouside the docroot.
   $config_directories[CONFIG_SYNC_DIRECTORY] = '../config';
+
   $settings['hash_salt'] = 'ivciasdbopasvbdcpasdiv';
-  $_ENV['AH_SITE_ENVIRONMENT'] = 'dev';
+
+  // set an enviroment variable to denote the environment status.
+  $_ENV['AH_SITE_ENVIRONMENT'] = 'loc'; $config['environment_indicator.indicator']['name'] = 'Local';
+  $_ENV['AH_SITE_ENVIRONMENT'] = 'dev'; $config['environment_indicator.indicator']['name'] = 'Development';
+  $_ENV['AH_SITE_ENVIRONMENT'] = 'stg'; $config['environment_indicator.indicator']['name'] = 'Staging';
+  $_ENV['AH_SITE_ENVIRONMENT'] = 'prd'; $config['environment_indicator.indicator']['name'] = 'Production';
+
+  // lock down site if it is tagged as production.
+  if (isset($_ENV['AH_SITE_ENVIRONMENT']) && $_ENV['AH_SITE_ENVIRONMENT'] === 'prd') {
+    $settings['config_readonly'] = TRUE;
+  }
+
+  // set the values for the environment_indicator module.
   $config['environment_indicator.indicator']['bg_color'] = '#cf0000'; //Red
   $config['environment_indicator.indicator']['fg_color'] = '#ffffff';
-  $config['environment_indicator.indicator']['name'] = 'Development';
   $config['shield.settings']['credentials']['shield']['user'] = NULL;
+
   $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/default/development.services.yml';
