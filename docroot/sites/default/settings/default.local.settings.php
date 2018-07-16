9a704b509ed1a8a8bf83f5d2dcf9cb8df5687f24
David <david.upton@boston.gov>
2018-07-16 19:29:34 -0400

    .

--- docroot/sites/default/settings/default.local.settings.php
+++ docroot/sites/default/settings/default.local.settings.php
@@ -65,3 +65,14 @@
   }
 
   $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/default/development.services.yml';
+if ($_SERVER['LANDO'] === 'ON') {
+  $lando_info = json_decode(getenv('LANDO_INFO'), TRUE);
+  $databases['default']['default'] = [
+    'driver' => 'mysql',
+    'database' => $lando_info['database']['creds']['database'],
+    'username' => $lando_info['database']['creds']['user'],
+    'password' => $lando_info['database']['creds']['password'],
+    'host' => $lando_info['database']['internal_connection']['host'],
+    'port' => $lando_info['database']['internal_connection']['port'],
+  ];
+}
