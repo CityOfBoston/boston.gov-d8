cdc9c6864b198e29db71eec69eaa9f60b314333e
David Upton <davidrkupton@users.noreply.github.com>
2018-07-17 00:59:29 -0400

    Initialize Testing processes and fix install bugs. (#7)
    * Adds base hooks to testing.xml tasks.
    * Fixes issues in lando start process.

--- docroot/sites/default/settings/default.local.settings.php
+++ docroot/sites/default/settings/default.local.settings.php
@@ -8,7 +8,6 @@
    * BOSTON.GOV NOTE: Database array is set by Phing script in setup.xml
    *    setup:drupal:local_settings_php:write
   */
-
   $settings['hash_salt'] = 'ivciasdbopasvbdcpasdiv';
 
   // set an enviroment variable to denote the environment status.
@@ -55,13 +54,27 @@
       break;
   }
 
-  // update the config if the environment variable has changed.
-  $config = \Drupal::service('config.factory')->getEditable('environment_indicator.indicator');
-  if ($config->get('name') != $env['name']) {
-    $config->set('name', $env['name']);
-    $config->set('bg_color', $env['bg_color']);
-    $config->set('fg_color', $env['v']);
-    $config->save();
-  }
+
 
   $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/default/development.services.yml';
+
+  if (!empty($_SERVER['LANDO']) && $_SERVER['LANDO'] === 'ON') {
+    $lando_info = json_decode(getenv('LANDO_INFO'), TRUE);
+    $databases['default']['default'] = [
+      'driver' => 'mysql',
+      'database' => $lando_info['database']['creds']['database'],
+      'username' => $lando_info['database']['creds']['user'],
+      'password' => $lando_info['database']['creds']['password'],
+      'host' => $lando_info['database']['internal_connection']['host'],
+      'port' => $lando_info['database']['internal_connection']['port'],
+    ];
+  }
+
+  //  update the config if the environment variable has changed.
+//  $config = \Drupal::service('config.factory')->getEditable('environment_indicator.indicator');
+//  if (isset($config) && $config->get('name') != $env['name']) {
+//    $config->set('name', $env['name']);
+//    $config->set('bg_color', $env['bg_color']);
+//    $config->set('fg_color', $env['fg_color']);
+//    $config->save();
+//  }
