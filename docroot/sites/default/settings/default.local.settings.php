6218dfd1bd4ef41bd5fbfd5c99d504f59f90c912
David <david.upton@boston.gov>
2018-07-17 00:21:31 -0400

    .

--- docroot/sites/default/settings/default.local.settings.php
+++ docroot/sites/default/settings/default.local.settings.php
@@ -8,7 +8,7 @@
    * BOSTON.GOV NOTE: Database array is set by Phing script in setup.xml
    *    setup:drupal:local_settings_php:write
   */
-
+  echo "aaa";
   $settings['hash_salt'] = 'ivciasdbopasvbdcpasdiv';
 
   // set an enviroment variable to denote the environment status.
@@ -54,26 +54,30 @@
       $settings['config_readonly'] = TRUE;
       break;
   }
-
-  // update the config if the environment variable has changed.
+  echo "bbb";
+  //   update the config if the environment variable has changed.
   $config = \Drupal::service('config.factory')->getEditable('environment_indicator.indicator');
+  echo "*";
   if ($config->get('name') != $env['name']) {
     $config->set('name', $env['name']);
     $config->set('bg_color', $env['bg_color']);
-    $config->set('fg_color', $env['v']);
+    $config->set('fg_color', $env['fg_color']);
     $config->save();
   }
+  echo "c";
 
   $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/default/development.services.yml';
+  echo "d";
 
   if ($_SERVER['LANDO'] === 'ON') {
+    echo "e";
     $lando_info = json_decode(getenv('LANDO_INFO'), TRUE);
-    $databases['default']['default'] = [
-      'driver' => 'mysql',
-      'database' => $lando_info['database']['creds']['database'],
-      'username' => $lando_info['database']['creds']['user'],
-      'password' => $lando_info['database']['creds']['password'],
-      'host' => $lando_info['database']['internal_connection']['host'],
-      'port' => $lando_info['database']['internal_connection']['port'],
-    ];
+//    $databases['default']['default'] = [
+//      'driver' => 'mysql',
+//      'database' => $lando_info['database']['creds']['database'],
+//      'username' => $lando_info['database']['creds']['user'],
+//      'password' => $lando_info['database']['creds']['password'],
+//      'host' => $lando_info['database']['internal_connection']['host'],
+//      'port' => $lando_info['database']['internal_connection']['port'],
+//    ];
   }
