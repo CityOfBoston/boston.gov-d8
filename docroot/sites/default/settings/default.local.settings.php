a48a004c5c6129d6e83ac78d6b1f18a99c396001
David Upton <davidrkupton@users.noreply.github.com>
2018-07-18 11:26:07 -0400

    David working (#8)
    Defines an initial set of core and migration-dependent modules.

--- docroot/sites/default/settings/default.local.settings.php
+++ docroot/sites/default/settings/default.local.settings.php
@@ -15,47 +15,6 @@
     $_ENV['AH_SITE_ENVIRONMENT'] = 'loc';
   }
 
-  switch ($_ENV['AH_SITE_ENVIRONMENT']) {
-    case "loc":
-      $env = [
-        'name' => 'Local',
-        'bg_color' => '#023e0a', //Green
-        'fg_color' => '#ffffff',
-      ];
-      break;
-
-    case 'dev':
-      $env = [
-        'name' => 'Development',
-        'bg_color' => '#3e0202', //Red
-        'fg_color' => '#ffffff',
-      ];
-      break;
-
-    case 'stg':
-      $env = [
-        'name' => 'Staging',
-        'bg_color' => '#505500', //Yellow
-        'fg_color' => '#ffffff',
-      ];
-      // lock down site if it is tagged as staging.
-      $settings['config_readonly'] = TRUE;
-      break;
-
-    case 'prd':
-    default:
-      $env = [
-        'name' => 'Production',
-        'bg_color' => '#303655', //blue
-        'fg_color' => '#ffffff',
-      ];
-      // lock down site if it is tagged as production.
-      $settings['config_readonly'] = TRUE;
-      break;
-  }
-
-
-
   $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/default/development.services.yml';
 
   if (!empty($_SERVER['LANDO']) && $_SERVER['LANDO'] === 'ON') {
@@ -69,12 +28,3 @@
       'port' => $lando_info['database']['internal_connection']['port'],
     ];
   }
-
-  //  update the config if the environment variable has changed.
-//  $config = \Drupal::service('config.factory')->getEditable('environment_indicator.indicator');
-//  if (isset($config) && $config->get('name') != $env['name']) {
-//    $config->set('name', $env['name']);
-//    $config->set('bg_color', $env['bg_color']);
-//    $config->set('fg_color', $env['fg_color']);
-//    $config->save();
-//  }
