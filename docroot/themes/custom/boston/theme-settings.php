4dfee6be13cec33f80163b252985346a35a08f9e
jimafisk <jimafisk@gmail.com>
2018-07-19 15:11:58 -0400

    Rename theme.

--- docroot/themes/custom/boston/theme-settings.php
+++ docroot/themes/custom/boston/theme-settings.php
@@ -8,7 +8,7 @@
 /**
  * Implements hook_form_system_theme_settings_alter().
  */
-function bos_theme_form_system_theme_settings_alter(&$form,  \Drupal\Core\Form\FormStateInterface $form_state, $form_id = NULL) {
+function boston_form_system_theme_settings_alter(&$form,  \Drupal\Core\Form\FormStateInterface $form_state, $form_id = NULL) {
   // Work-around for a core bug affecting admin themes. See issue #943212.
   if (isset($form_id)) {
     return;
