16bff3e152e51193d94d5f2752e17f12bb263532
David Upton <davidrkupton@users.noreply.github.com>
2018-08-14 09:33:58 -0400

    David monday (#99)
    * Adds patterns server url for staging.
    * Converting html and snippits templates to twigs.
    * Adds config split to manage development settings
    * Adds development modules to .config/develop
    * Adds logging to the bos_profile.install module-switching process.
    * Fixes branch selection for private repo.
    * Adds config split to manage production settings.
    * Adds production modules to .config/prod. Adds language module configs.
    * Updates bos_profile.install with d8 style config commands.
    * Changes production and development config_splits.
    * Adds modules to correct Develop and Production config-splits.
    * Removes module dependency/installs except those in core.extension.yml.
    * Fixes error in bos_theme.theme.
    * Adds some required modules, resets bos_admin base to adminimal_theme.
    * Resets config_splits settings files after setup completes.
    * Resets configs.
    * Partially sets unwanted config_splits to disabled.
    * Finishes setting unwanted config_splits to disabled.

--- docroot/sites/default/settings/default.local.settings.php
+++ docroot/sites/default/settings/default.local.settings.php
@@ -15,7 +15,9 @@
   /*  @see https://docs.acquia.com/acquia-cloud/develop/env-variable/ */
   global $envvar;
   if (empty($_ENV['AH_SITE_ENVIRONMENT'])) {
-    $envvar = getenv('AH_SITE_ENVIRONMENT');
+    if (!empty(getenv('AH_SITE_ENVIRONMENT'))) {
+      $envvar = getenv('AH_SITE_ENVIRONMENT');
+    }
   }
   else {
     $envvar = $_ENV['AH_SITE_ENVIRONMENT'];
@@ -23,8 +25,6 @@
   if (empty($envvar)) {
     $envvar = 'dev';
   }
-
-  $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/default/development.services.yml';
 
   if (!empty($_SERVER['LANDO']) && $_SERVER['LANDO'] === 'ON') {
     $lando_info = json_decode(getenv('LANDO_INFO'), TRUE);
@@ -39,3 +39,27 @@
   }
 
   $settings['file_private_path'] = 'sites/default/files/private';
+
+  // Add in the development services config file.
+  if ($envvar == "dev") {
+    $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/default/development.services.yml';
+  }
+
+  /**
+  * Toggle the development settings in config_split
+   * (Now in profile_install)
+  */
+//  $config['config_split.config_split.development']['status'] = ($envvar == "dev");
+
+  /**
+  * Set the error trapping level.
+  *   options: hide | some | all | verbose
+  */
+  $config['system.logging']['error_level'] = ($envvar == "dev" ? 'verbose' : 'hide');
+
+  /**
+   * Disable CSS and JS aggregation.
+   * (Now in profile_install)
+   */
+//  $config['system.performance']['css']['preprocess'] = !($envvar == "dev");
+//  $config['system.performance']['js']['preprocess'] = !($envvar == "dev");
