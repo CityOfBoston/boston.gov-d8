b709ea6ca9733a23a945ed3416c82d9cfe4e7456
David <david.upton@boston.gov>
2018-08-24 15:09:33 -0400

    Adds Travis to the repo.

--- scripts/phing/task.sh
+++ scripts/phing/task.sh
@@ -9,4 +9,4 @@ if [ ! -f ${DIR}/vendor/bin/phing ]; then
 fi
 
 # This script simply passes all arguments to Phing.
-${DIR}/vendor/bin/phing -f ${DIR}/build.xml "$@"
+${DIR}/vendor/bin/phing -f ${DIR}/phing-boston.xml "$@"
