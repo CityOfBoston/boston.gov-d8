d60f5ee2978be7f1a26f383625ca09b8dcb77c23
David Upton <davidrkupton@gmail.com>
2018-12-24 12:13:59 -0500

    Moves post deploy and update tasks from phing into acquia hooks.

---
+++ hooks/prod/post-code-deploy/post-code-deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#
+# Cloud Hook: post-code-deploy
+#
+# The post-code-deploy hook is run whenever you use the Workflow page to 
+# deploy new code to an environment, either via drag-drop or by selecting
+# an existing branch or tag from the Code drop-down list. See 
+# ../README.md for details.
+#
+# Usage: post-code-deploy site target-env source-branch deployed-tag repo-url 
+#                         repo-type
+
+site="$1"
+target_env="$2"
+source_branch="$3"
+deployed_tag="$4"
+repo_url="$5"
+repo_type="$6"
+drush_alias="@bostond8.prod"
+
+if [ "$source_branch" != "$deployed_tag" ]; then
+    echo "$site.$target_env: Deployed branch $source_branch as $deployed_tag.  Running post-deploy tasks."
+
+    echo "Import configurations from $source_branch."
+    /usr/local/drush8/drush $drush_alias cim -y
+
+    echo "Update $site.$target_env DB with un-executed hook_updates on deploy target."
+    /usr/local/drush8/drush $drush_alias updb -y
+
+    echo "Update existing entities on $site.$target_env (poss. redundant)."
+    /usr/local/drush8/drush $drush_alias entup -y
+
+    echo "Rebuild user content access permissions on $site.$target_env."
+    /usr/local/drush8/drush $drush_alias php-eval 'node_access_rebuild()' -y
+
+else
+    echo "$site.$target_env: Deployed $deployed_tag."
+fi
---
+++ hooks/test/post-code-update/post-code-update.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+#
+# Cloud Hook: post-code-update
+#
+# The post-code-update hook runs in response to code commits.
+# When you push commits to a Git branch, the post-code-update hooks runs for
+# each environment that is currently running that branch.. See
+# ../README.md for details.
+#
+# Usage: post-code-update site target-env source-branch deployed-tag repo-url
+#                         repo-type
+
+site="$1"
+target_env="$2"
+source_branch="$3"
+deployed_tag="$4"
+repo_url="$5"
+repo_type="$6"
+drush_alias="@bostond8.test"
+
+if [ "$target_env" = 'dev' ]; then
+    echo "$site.$target_env: The $source_branch branch has been updated on $target_env. Running post-update tasks.."
+
+    echo "Remove config_split module."
+    /usr/local/drush8/drush $drush_alias pmu config_split -y
+    /usr/local/drush8/drush $drush_alias cdel core.extension module.config_split -y
+
+    echo "Import configurations from $source_branch."
+    /usr/local/drush8/drush $drush_alias cim -y
+
+    #    DEV ONLY - enable migration modules.
+    echo "DEVELOPMENT PRE-D8 LAUNCH ONLY: Add back in some dev modules."
+    /usr/local/drush8/drush $drush_alias en migrate -y
+
+    echo "Update $site.$target_env DB with un-executed hook_updates on deploy target."
+    /usr/local/drush8/drush $drush_alias updb -y
+
+    echo "Update existing entities on $site.$target_env (poss. redundant)."
+    /usr/local/drush8/drush $drush_alias entup -y
+
+    echo "Rebuild user content access permissions on $site.$target_env."
+    /usr/local/drush8/drush $drush_alias php-eval 'node_access_rebuild()' -y
+
+    echo "Run cron now on $site.$target_env (poss. redundant)."
+    /usr/local/drush8/drush $drush_alias cron
+else
+    echo "$site.$target_env: The $source_branch branch has been updated on $target_env."
+fi
