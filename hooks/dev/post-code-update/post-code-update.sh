f15f19a8ee9f29096c50720a75d4e2c3210b64d8
David Upton <davidrkupton@gamil.com>
2018-12-24 23:37:17 -0500

    Corrects drush command in hooks for dev code_update.

--- hooks/dev/post-code-update/post-code-update.sh
+++ hooks/dev/post-code-update/post-code-update.sh
@@ -17,32 +17,33 @@ deployed_tag="$4"
 repo_url="$5"
 repo_type="$6"
 drush_alias="@bostond8.dev"
+target_docroot="/var/www/html/bostond8.dev/docroot"
 
 if [ "$target_env" = 'dev' ]; then
     echo "$site.$target_env: The $source_branch branch has been updated on $target_env. Running post-update tasks."
 
     echo "Remove config_split module."
-    cd /var/www/html/bostond8.dev/docroot && drush9 $drush_alias pmu config_split -y
-    cd /var/www/html/bostond8.dev/docroot && drush9 $drush_alias cdel core.extension module.config_split -y
+    cd ${target_docroot} && drush9 $drush_alias pmu config_split -y
+    cd ${target_docroot} && drush9 $drush_alias cdel core.extension module.config_split -y
 
     echo "Import configurations from $source_branch."
-    cd /var/www/html/bostond8.dev/docroot && drush9 $drush_alias cim -y
+    cd ${target_docroot} && drush9 $drush_alias cim -y
 
     #    DEV ONLY - enable migration modules.
     echo "DEVELOPMENT PRE-D8 LAUNCH ONLY: Add back in some dev modules."
-    cd /var/www/html/bostond8.dev/docroot && drush9 $drush_alias en migrate -y
+    cd ${target_docroot} && drush9 $drush_alias en migrate -y
 
     echo "Update $site.$target_env DB with un-executed hook_updates on deploy target."
-    cd /var/www/html/bostond8.dev/docroot && drush9 $drush_alias updb -y
+    cd ${target_docroot} && drush9 $drush_alias updb -y
 
     echo "Update existing entities on $site.$target_env (poss. redundant)."
-    cd /var/www/html/bostond8.dev/docroot && drush9 $drush_alias entup -y
+    cd ${target_docroot} && drush9 $drush_alias entup -y
 
     echo "Rebuild user content access permissions on $site.$target_env."
-    cd /var/www/html/bostond8.dev/docroot && drush9 $drush_alias php-eval 'node_access_rebuild()' -y
+    cd ${target_docroot} && drush9 $drush_alias php-eval 'node_access_rebuild()' -y
 
     echo "Run cron now on $site.$target_env (poss. redundant)."
-    cd /var/www/html/bostond8.dev/docroot && drush9 $drush_alias cron
+    cd ${target_docroot} && drush9 $drush_alias cron
 
 else
     echo "$site.$target_env: The $source_branch branch has been updated on $target_env."
