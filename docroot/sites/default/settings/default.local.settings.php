01372009d7233d470156b8bdf70713cc79e97f6e
David Upton <davidrkupton@users.noreply.github.com>
2018-12-26 15:04:12 -0500

    Creates Acquia deployment via Travis. (#199)
    Adds functionality to build on Travis and deploy to Acquia.

--- docroot/sites/default/settings/default.local.settings.php
+++ docroot/sites/default/settings/default.local.settings.php
@@ -10,22 +10,6 @@
 
 $settings['hash_salt'] = 'ivciasdbopasvbdcpasdiv';
 
-// Be sure an environment indicator is set.
-// Note: on Acquia servers this will be one of prod / test / dev as per:
-/*  @see https://docs.acquia.com/acquia-cloud/develop/env-variable/ */
-global $_envvar;
-if (empty($_ENV['AH_SITE_ENVIRONMENT'])) {
-  if (!empty(getenv('AH_SITE_ENVIRONMENT'))) {
-    $_envvar = getenv('AH_SITE_ENVIRONMENT');
-  }
-}
-else {
-  $_envvar = $_ENV['AH_SITE_ENVIRONMENT'];
-}
-if (empty($_envvar)) {
-  $_envvar = 'dev';
-}
-
 if (!empty($_SERVER['LANDO']) && $_SERVER['LANDO'] === 'ON') {
   $lando_info = json_decode(getenv('LANDO_INFO'), TRUE);
   $databases['default']['default'] = [
@@ -37,19 +21,18 @@ if (!empty($_SERVER['LANDO']) && $_SERVER['LANDO'] === 'ON') {
     'port' => $lando_info['database']['internal_connection']['port'],
   ];
 }
-
-$settings['file_private_path'] = 'sites/default/files/private';
-
-// Add in the development services config file.
-if ($_envvar == "dev") {
-  $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/default/development.services.yml';
+else {
+  $databases['default']['default'] = [
+    'driver' => 'mysql',
+    'database' => 'drupal',
+    'username' => 'drupal',
+    'password' => 'drupal',
+    'host' => 'database',
+    'port' => '3306',
+  ];
 }
 
-/*
- * Set the error trapping level.
- *   options: hide | some | all | verbose
- */
-$config['system.logging']['error_level'] = ($_envvar == "dev" ? 'verbose' : 'hide');
+$settings['file_private_path'] = 'sites/default/files/private';
 
 /*
  * Add PHP memory limit allocation for this web site.
