a8979a94e3031fd1ef2b1e42103c2ff98d63077d
David Upton <davidrkupton@gmail.com>
2019-01-25 16:10:07 -0500

    Adds REST tracking in Google Analytics. Also Form and Drush

---
+++ docroot/modules/custom/bos_core/src/BosCoreGAPost.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * @see https://developers.google.com/analytics/devguides/collection/protocol/v1/reference
+ */
+
+namespace Drupal\bos_core;
+
+use Drupal\Core\Config\ConfigFactory;
+use Drupal\Core\Logger\LoggerChannelFactory;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\TransferException;
+use Psr\Container\ContainerInterface;
+
+/**
+ * Class BosCoreGAPost.
+ *
+ *    Posts to the Google Analytics vX endpoint.
+ *
+ * @package Drupal\bos_core
+ */
+class BosCoreGAPost {
+
+  private $log;
+
+  /**
+   * BosCoreGAPost create.
+   *
+   * @inheritdoc
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('logger.factory'),
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * BosCoreGAPost constructor.
+   *
+   * @inheritdoc
+   */
+  public function __construct(LoggerChannelFactory $logger, ConfigFactory $config) {
+    $this->log = $logger->get('EmergencyAlerts');
+    $this->config = $config->get("bos_core.settings");
+  }
+
+  /**
+   * Logs API actions to Google Analytics.
+   *
+   * @param string $page_id
+   *   This is the document path for API tracking.
+   * @param string $page_title
+   *   This is the document title for API tracking.
+   *
+   *   Format: api/endpoint_module/type/description.
+   *   E.g. api/cityscore/list/summary.
+   *   E.g. api/cityscore/list/html.
+   *
+   * @return boolean
+   *    True if posted OK else false.
+   * @throws \GuzzleHttp\Exception\GuzzleException
+   */
+  public static function pageview(string $page_id, string $page_title = NULL) {
+    $settings = \Drupal::config("bos_core.settings")->get("bos_core.settings");
+
+    if(!$settings["ga_enabled"]) {
+      return;
+    }
+
+    if (!isset($page_title)) {
+      $page_title = "CoB REST | " . str_replace("/", "-", trim($page_id, "/api/"));
+    }
+
+    $payload = [
+      "v" => 1,
+      "tid" => $settings["ga_tid"],
+      "t" => "pageview",
+      "dp" => $page_id,
+      "dt" => $page_title,
+      "ni" => 1,
+      "cid" => $settings["ga_cid"],
+      "uip" => \Drupal::request()->getClientIp(),
+      "cg1" => "API",
+    ];
+    foreach ($payload as $key => &$value) {
+      $value = utf8_encode($value);
+      $value = urlencode($value);
+    }
+
+    $client = new Client();
+
+    $endpoint = isset($settings["ga_endpoint"]) ? $settings["ga_endpoint"] : "https://www.google-analytics.com/collect";
+
+    try {
+      $client->request('GET', $endpoint , [
+        'query' => $payload,
+        'headers' => [
+          "Content-type: text/plain",
+        ]
+      ]);
+      return TRUE;
+
+    }
+    catch (TransferException | \Exception $except) {
+      // static function so cannot use $this->>log() ...
+      \Drupal::logger("Boston Core")->error("Google Analytics Post error.", []);
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * Return the endpoint currently set in configs and being used by this class.
+   *
+   * @return string
+   *   The currently set endpoint.
+   */
+  public static function endpoint() {
+    $settings = \Drupal::config("bos_core.settings")->get("bos_core.settings");
+    return $settings["ga_endpoint"];
+  }
+
+}
---
+++ docroot/modules/custom/bos_core/src/Form/BosCoreSettingsForm.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * @see https://developers.google.com/analytics/devguides/collection/protocol/v1/reference
+ */
+
+namespace Drupal\bos_core\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class BosCoreSettingsForm.
+ *
+ * @package Drupal\bos_core\Form
+ */
+
+class BosCoreSettingsForm extends ConfigFormBase {
+
+  /**
+   * Implements getFormId().
+   */
+  public function getFormId() {
+    return 'bos_core_admin_settings';
+  }
+
+  /**
+   * Implements getEditableConfigNames().
+   */
+  protected function getEditableConfigNames() {
+    return ["bos_core.settings"];
+  }
+
+  /**
+   * Implements buildForm()
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('bos_core.settings');
+    $settings = $config->get('ga_settings');
+
+    $endpoint = isset($settings["ga_endpoint"]) ? $settings["ga_endpoint"] : "https://www.google-analytics.com/collect";
+
+    $form = [
+      '#tree' => TRUE,
+      'bos_core' => [
+        '#type' => 'fieldset',
+        '#title' => 'Boston Core Settings',
+        '#description' => 'Configuration for Core Boston Components.',
+        '#collapsible' => FALSE,
+
+        "ga_settings" => [
+          '#type' => 'details',
+          '#title' => 'Google Analytics',
+          '#description' => 'Configuration for REST endpoint tracking in Google Analytics.',
+          '#open' => TRUE,
+
+          "ga_enabled" => [
+            '#type' => 'checkbox',
+            '#title' => t('REST Tracking Enabled'),
+            '#default_value' => isset($settings['ga_enabled']) ? $settings['ga_enabled'] : FALSE,
+          ],
+
+          'ga_endpoint' => [
+            '#type' => 'textfield',
+            '#title' => t('Google URL Endpoint'),
+            '#description' => t('This is the Google Measurement API endpoint being used.<br/>
+                  This value can only be changed using drush (drush bgae) - please contact a developer to make changes.'),
+            '#default_value' => $endpoint,
+            '#attributes' => [
+              "disabled" => TRUE,
+            ],
+          ],
+          'ga_tid' => [
+            '#type' => 'textfield',
+            '#title' => t('Tracking ID'),
+            '#description' => t('Enter the Google Tracking Id provided by Google.'),
+            '#default_value' => $settings['ga_tid'],
+            '#attributes' => [
+              "placeholder" => 'e.g. UA-XXXXXXX-XX',
+            ],
+            '#required' => TRUE,
+          ],
+          "ga_cid" => [
+            '#type' => 'textfield',
+            '#title' => t('Client ID'),
+            '#description' => t("Enter a Client Id (32char Hex). <br/> See <a href='https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters#cid'>documentation</a>"),
+            '#default_value' => $settings['ga_cid'],
+            '#attributes' => [
+              "placeholder" => 'e.g. 35009a79-1a05-49d7-b876-2b884d0f825b',
+            ],
+            '#required' => TRUE,
+          ]
+       ]
+     ]
+   ];
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * Implements submitForm().
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $settings = $form_state->getValue('bos_core');
+
+    $newValues = [
+      'ga_tid' => $settings['ga_settings']['ga_tid'],
+      'ga_cid' => $settings['ga_settings']['ga_cid'],
+      'ga_enabled' => $settings['ga_settings']['ga_enabled'],
+    ];
+
+    $this->config('bos_core.settings')
+      ->set('ga_settings', $newValues)
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
