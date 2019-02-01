dfa75dbda77be667ed96f4f26c751e018e546f51
David Upton <davidrkupton@gmail.com>
2019-01-31 19:23:14 -0500

    Converts site_alert content type.

---
+++ docroot/modules/custom/bos_content/modules/site_alert/js/site-alert.boston.js
@@ -0,0 +1,64 @@
+/**
+ * @file
+ * Javascript to check for alerts.
+ */
+
+  'use strict';
+
+  var BostonAlert = (function () {
+    var el = document.getElementById('sa');
+    var seal = document.getElementById('seal');
+    var exclude = true;
+
+    function getAlert() {
+      var request = new XMLHttpRequest();
+
+      request.open('GET',
+        '/api/v1/alerts/site',
+        true);
+
+      request.onload = function () {
+        if (request.status >= 200 && request.status < 400) {
+          var resp = request.responseText;
+
+          if (resp.length > 0) {
+            var classes = el.getAttribute('data-classes');
+            var target = el.getAttribute('data-target');
+
+            el.innerHTML = resp;
+
+            var alert = document.getElementById('sa-a');
+            el.innerHTML = document.getElementById('sa').innerHTML;
+
+            if (alert) {
+              var excludes = alert.getAttribute('data-excludes');
+
+              if (target === ""  ||  excludes.indexOf(target) == -1) {
+                exclude = false;
+                el.style.display = 'block';
+                document.getElementById('page').className = classes;
+              }
+            }
+          }
+
+          if (exclude) {
+            el.parentElement.removeChild(el);
+          }
+          else {
+            seal.parentElement.removeChild(seal);
+          }
+
+          var script = document.getElementById('alert_script');
+          script.parentElement.removeChild(script);
+        }
+      };
+
+      request.send();
+    }
+
+    return {
+      getAlert: getAlert
+    }
+  })();
+
+  BostonAlert.getAlert();
