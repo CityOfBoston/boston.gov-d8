3f06f98ce430452a9ad8290f3747fa11fddd7a2f
David Upton <davidrkupton@gmail.com>
2019-03-01 17:23:44 -0500

    Adds a secondary set of action buttons and manages display.

---
+++ docroot/themes/custom/bos_admin/js/bos_admin.actions.js
@@ -0,0 +1,26 @@
+(function ($, window, document) {
+  'use strict';
+  $(document).ready(function () {
+    var actions = $(".bos_admin .layout-region-node-main #edit-actions");
+    if (actions.length) {
+      var showActions = function () {
+        $(".bos_admin .vertical-tabs__pane").each(function () {
+          var blockTop = $(".bos_admin .layout-region-node-main").position().top;
+          if($(this).is(":visible")) {
+
+            if (($(this).height() + blockTop) > $(window).height()) {
+              actions.slideDown();
+            }
+            else {
+              actions.slideUp();
+            }
+          }
+        });
+      };
+      $(".bos_admin .vertical-tabs__menu-item").click(function () {
+        showActions();
+      });
+      showActions();
+    }
+  });
+})(jQuery, this, this.document);
