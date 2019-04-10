c83b579c995157169a3ad09990547ba1054f889e
David Upton <davidrkupton@gmail.com>
2019-04-10 12:32:02 -0400

    Codesniff.

--- docroot/modules/custom/bos_content/modules/node_emergency_alert/js/emergency_alert.boston.js
+++ docroot/modules/custom/bos_content/modules/node_emergency_alert/js/emergency_alert.boston.js
@@ -1,16 +1,20 @@
-(function($) {
+/**
+ * @file
+ */
+
+(function ($) {
     Drupal.behaviors.bosContentTypeEmergencyAlert = {
-        'attach': function(context) {
-            $('#statusHeader').once("emergency-override", function() {
-                $.get( "/emergency-alert?" + Math.round(new Date().getTime() / 1000), null, callback);
+        'attach': function (context) {
+            $('#statusHeader').once("emergency-override", function () {
+                $.get("/emergency-alert?" + Math.round(new Date().getTime() / 1000), null, callback);
             });
         }
     };
 
-    var callback = function(response) {
-        if(response.emergency) {
+    var callback = function (response) {
+        if (response.emergency) {
             var element = $('#statusHeader');
             element.html(response.html);
         }
-    }
+    };
 })(jQuery);
