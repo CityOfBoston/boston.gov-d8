87c247a983127fd417f079373293869a344d72a9
David Upton <davidrkupton@gmail.com>
2019-04-10 17:20:27 -0400

    Updates ui context edit menu text.

--- docroot/themes/custom/bos_theme/js/bos_theme.context_menu_link.js
+++ docroot/themes/custom/bos_theme/js/bos_theme.context_menu_link.js
@@ -8,37 +8,37 @@
 (function ($, window, document) {
   'use strict';
   $(document).ready(function () {
-    var links = $("button.trigger").parent().find("[class^=entitynode]");
-    var type = "Node";
-
-    /*
-      Attempts to determine what sort of node this is from the classes applied.
-     */
-    var findNode = function () {
-      var classes = links.parents("article").attr("class").split(/\s+/);
-      $(classes).each(function (key, value) {
-        if (value == "node-article") {
-          type = "Article";
-        }
-      });
-    };
+    // Var contextButtons are a list of context buttons on the page.
+    var contextButtons = $("button.trigger"); //.parent().find("[class^=entitynode]");
+    // Create a default type for dropdowns.
+    var type = "Element";
 
     // Adds a tooltip to the button, changes the click funtion of the contextual button when
     // only one link and adds the node type to the links list.
-    if (links.length == 1) {
-      findNode();
+    if (contextButtons.length == 1) {
+      var links = contextButtons.parent().find("[class^=entitynode]");
+      if (links.length == 0 ) {
+        links = contextButtons.parent().find("[class^=paragraphs]");
+      }
       links.parents("ul").hide();
       links.parents(".contextual").attr({"title": "Click to Edit " + type + "."});
       links.parent().parent().click(function () {
         window.location.href = links.first().find("a").prop("href");
       });
     }
-    else if (links.length > 1) {
-      findNode();
-      links.each(function (key, listItem) {
-        $(listItem).find("a").text($(listItem).find("a").text() + " " + type);
+    else if (contextButtons.length > 1) {
+      contextButtons.each(function (key, button) {
+        type = $(button).parents(".contextual-region").attr("bos_context_type");
+        if (typeof type === "undefined") {type = "Element";}
+        var links = $(button).parent().find("[class^=entitynode]");
+        if (links.length == 0 ) {
+          links = contextButtons.parent().find("[class^=paragraphs]");
+        }
+        links.each(function (key, listItem) {
+          $(listItem).find("a").text($(listItem).find("a").text() + " " + type);
+        });
+        links.parents(".contextual").attr({"title": "Click for " + type + " admin options."});
       });
-      links.parents(".contextual").attr({"title": "Click for " + type + " admin options."});
     }
   });
 })(jQuery, this, this.document);
