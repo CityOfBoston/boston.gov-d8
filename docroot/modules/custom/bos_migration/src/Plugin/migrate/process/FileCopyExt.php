9282c616d46ccaba1103052611f28962e4f0d136
David Upton <davidrkupton@gmail.com>
2019-05-07 14:33:08 -0400

    Overwrites and extends the fileCopy migration. - Adds move & copy configs to control/prevent file ops during migrate.

--- docroot/modules/custom/bos_migration/src/Plugin/migrate/process/FileCopyExt.php
+++ docroot/modules/custom/bos_migration/src/Plugin/migrate/process/FileCopyExt.php
@@ -3,9 +3,9 @@
 
 namespace Drupal\bos_migration\Plugin\migrate\process;
 
-use Drupal\migrate\MigrateException;
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\Plugin\migrate\process\FileCopy;
+use Drupal\migrate\Plugin\MigrationInterface;
 use Drupal\migrate\Row;
 
 /**
@@ -22,22 +22,55 @@ use Drupal\migrate\Row;
  *
  * @MigrateProcessPlugin(
  *   id = "file_copy_ext"
+ * )
  */
+
 class FileCopyExt extends FileCopy {
 
   /**
    * Extend the actual copy action to squash file-not-found errors.
    *
+   * Replaces the fileCopy->transform funcion.
+   *
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
-    try {
-      $result = parent::transform($value, $migrate_executable, $row, $destination_property);
+
+    list($source, $destination) = $value;
+
+    // If we don't have an actual file action, then dont do anything.
+    if (!empty($this->configuration['move']) || !empty($this->configuration['copy'])) {
+      return $destination;
     }
-    catch(MigrateException $e) {
-      // squash a file not found error.
-      return "/dev/null";
+
+    if (!file_exists($source)) {
+      $fid=$row->getSource()['fid'];
+      $migrate_executable->saveMessage("File (fid:$fid) '$source' does not exist", MigrationInterface::MESSAGE_NOTICE);
+      return $destination;
     }
+
+    return parent::transform($value, $migrate_executable, $row, $destination_property);
+
+  }
+
+  /**
+   * Tries to move or copy a file.  Extended to do neither.
+   *
+   * Replaces the fileCopy->writeFile funcion.
+   *
+   * {@inheritdoc}
+   */
+  protected function writeFile($source, $destination, $replace = FILE_EXISTS_REPLACE) {
+    // Check if there is a destination available for copying. If there isn't,
+    // it already exists at the destination and the replace flag tells us to not
+    // replace it. In that case, return the original destination.
+    if (!($final_destination = file_destination($destination, $replace))) {
+      return $destination;
+    }
+    if (!empty($this->configuration['move']) || !empty($this->configuration['copy'])) {
+      $function = 'file_unmanaged_' . ($this->configuration['move'] ? 'move' : 'copy');
+    }
+    return $function($source, $destination, $replace);
   }
 
 }
