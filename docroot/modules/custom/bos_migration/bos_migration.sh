af1bd6b84af01435f5be512b790f970522d19df9
David Upton <davidrkupton@gmail.com>
2019-05-24 16:47:28 -0400

    Updates from debugging migration end-to-end.

--- docroot/modules/custom/bos_migration/bos_migration.sh
+++ docroot/modules/custom/bos_migration/bos_migration.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Lowest level safe-dependencies.
 drush sset "bos_migration.fileOps" "none"
+drush sset "bos_migration.active" "1"
 drush mim --tag="bos:initial:1" --force     #OK
 
 # First pass.
@@ -12,6 +13,7 @@ drush mim --tag="bos:paragraph:2" --force # deps:tax/fc/para1
 drush mim --tag="bos:paragraph:3" --force # deps:para2
 #drush mim --tag="bos:paragraph:4" --force # Components/sidebar-components
 drush mim --tag="bos:node:1" --force
+drush mim --tag="bos:node:2" --force
 drush mim --tag="bos:paragraph:5" --force # deps:nodes
 # Second pass.
 drush mim --group=d7_taxonomy_term --update
@@ -20,3 +22,6 @@ drush mim --group=d7_nodes --update
 # Finish off.
 drush mim d7_menu_links,d7_menu,d7_block
 drush mim d7_rdf_mapping
+
+drush sdel "bos_migration.active"
+drush sset "bos_migration.fileOps" "copy"
