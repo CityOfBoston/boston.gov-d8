e1b36b4b5890a169437ba753f08801e47902b322
David Upton <davidrkupton@gmail.com>
2019-06-01 14:09:07 -0400

    Fixes html parsing issue on loads of rich-text fields.

--- docroot/modules/custom/bos_migration/src/HtmlParsingTrait.php
+++ docroot/modules/custom/bos_migration/src/HtmlParsingTrait.php
@@ -27,12 +27,19 @@ trait HtmlParsingTrait {
     // Skipping them is safe, because Drupal will anyway filter the string on
     // database during render.
     set_error_handler(function (int $errno, string $errstr) use ($migrate_executable) {
+      // DU: Added LIBXML_NOERROR (see below), so we should never end up here.
+      // Problem is that this does not simply log the error, it stops the
+      // $document from being returned and hence the field is not migrated ...
       $migrate_executable->saveMessage($errstr, MigrationInterface::MESSAGE_NOTICE);
     });
     // Prepend the html with a header to use UTF-8 as source enconding.
     // By default loadHTML() assumes ISO-8859-1.
     $html = '<?xml encoding="UTF-8">' . $html;
-    $document->loadHTML($html);
+    // Use option LIBXML_NOERROR to avoid random html parsing errors.
+    // (we can broadly assume html is legit enough).
+    // @see https://www.php.net/manual/en/domdocument.loadhtml.php and,
+    // @see https://www.php.net/manual/en/libxml.constants.php
+    $document->loadHTML($html, LIBXML_NOERROR);
     restore_error_handler();
     return $document;
   }
