63d1a55b37dc45bba1666b2eb8789ffd1931aa98
David Upton <davidrkupton@gmail.com>
2019-06-27 15:02:52 -0400

    Improves file relocation during migration.

--- docroot/modules/custom/bos_migration/src/FilesystemReorganizationTrait.php
+++ docroot/modules/custom/bos_migration/src/FilesystemReorganizationTrait.php
@@ -56,36 +56,43 @@ trait FilesystemReorganizationTrait {
    *   Array mapping regex old folder name to a new folder.
    */
   protected $folderMappings = [
-    "~department\-icon\-([0-9]*)\-([0-9]*)~" => "img/icons/department/$2/$1",
-    "~department_icons~" => "img/icons/department",
-    "~fyi\-icon\-([0-9]*)\-([0-9]*)~" => "img/icons/fyi/$2/$1",
-    "~fyi\-icon~" => "img/icons/fyi",
-    "~paragraphs_type_icon~" => "img/icons/paragraphs_type",
-    "~status_icons~" => "img/icons/status",
-    "~status\-item\-icon\-([0-9]*)\-([0-9]*)~" => "img/icons/status_icon/$2/$1",
-    "~transactions\-icon\-([0-9]*)\-([0-9]*)~" => "img/icons/transactions/$2/$1",
-    "~document\-file\-([0-9]*)\-([0-9]*)~" => "file/document_files/$2/$1",
-    "~event_intro_images~" => "img/event/intro_images",
-    "~event\-intro\-images\-([0-9]*)\-([0-9]*)~" => "img/event/intro_images/$2/$1",
-    "~event\-thumbnail\-([0-9]*)\-([0-9]*)~" => "img/event/thumbnails/$2/$1",
-    "~field\-columns\-image\-([0-9]*)\-([0-9]*)~" => "img/field_columns/$2/$1",
-    "~hero\-image\-([0-9]*)\-([0-9]*)~" => "img/hero_image/$2/$1",
-    "~how\-to\-intro\-image\-([0-9]*)\-([0-9]*)~" => "img/how_to/intro_images/$2/$1",
-    "~how_to_intro_images~" => "img/how_to/intro_images",
-    "~listing\-page\-intro\-image\-([0-9]*)\-([0-9]*)~" => "img/listing_page/intro_images/$2/$1",
-    "~listing_page_intro_images~" => "img/listing_page/intro_images",
-    "~person\-profile\-photo\-([0-9]*)\-([0-9]*)~" => "img/person_profile/photos/$2/$1",
-    "~photo\-image\-([0-9]*)\-([0-9]*)~" => "img/library/photos/$2/$1",
-    "~place\-profile\-intro\-image\-([0-9]*)\-([0-9]*)~" => "img/place_profile/intro_images/$2/$1",
-    "~post\-intro\-image\-([0-9]*)\-([0-9]*)~" => "img/post/intro_images/$2/$1",
-    "~post\-thumbnail\-([0-9]*)\-([0-9]*)~" => "img/post/thumbnails/$2/$1",
-    "~program\-intro\-image\-([0-9]*)\-([0-9]*)~" => "img/program/intro_images/$2/$1",
-    "~program\-logo\-([0-9]*)\-([0-9]*)~" => "img/program/logo/$2/$1",
-    "~quote\-person\-photo\-([0-9]*)\-([0-9]*)~" => "img/quote_person/photos/$2/$1",
-    "~tabbed\-intro\-image\-([0-9]*)\-([0-9]*)~" => "img/tabbed/intro_images/$2/$1",
-    "~topic\-intro\-image\-([0-9]*)\-([0-9]*)~" => "img/topic/intro_images/$2/$1",
-    "~topic\-thumbnail\-([0-9]*)\-([0-9]*)~" => "img/topic/thumbnails/$2/$1",
-    "~video\-image\-([0-9]*)\-([0-9]*)~" => "img/video/$2/$1",
+    "~//intro_images~" => "//img/unk/intro_images",
+    "~//department\-icon\-([0-9]*)\-([0-9]*)~" => "//img/icons/department/$2/$1",
+    "~//department_icons~" => "//img/icons/department",
+    "~//fyi\-icon\-([0-9]*)\-([0-9]*)~" => "//img/icons/fyi/$2/$1",
+    "~//fyi\-icon~" => "//img/icons/fyi",
+    "~//paragraphs_type_icon~" => "//img/icons/paragraphs_type",
+    "~//status_icons~" => "//img/icons/status",
+    "~//status\-item\-icon\-([0-9]*)\-([0-9]*)~" => "//img/icons/status/$2/$1",
+    "~//transactions\-icon\-([0-9]*)\-([0-9]*)~" => "//img/icons/transactions/$2/$1",
+    "~//default_images~" => "//file/document_files/unk",
+    "~//document\-file\-([0-9]*)\-([0-9]*)~" => "//file/document_files/$2/$1",
+    "~//document\-file\-([0-9]{4})([0-9]{2})[0-9]{2}~" => "//file/document_files/$1/$2",
+    "~//event_intro_images~" => "//img/event/intro_images",
+    "~//event\-intro\-image(s|)\-([0-9]*)\-([0-9]*)~" => "//img/event/intro_images/$3/$2",
+    "~//event\-thumbnail\-([0-9]*)\-([0-9]*)~" => "//img/event/thumbnails/$2/$1",
+    "~//feature_icons~" => "//img/icons/feature",
+    "~//field\-columns\-image\-([0-9]*)\-([0-9]*)~" => "//img/columns/$2/$1",
+    "~//hero\-image\-([0-9]*)\-([0-9]*)~" => "//img/hero_image/$2/$1",
+    "~//how\-to\-intro\-image\-([0-9]*)\-([0-9]*)~" => "//img/how_to/intro_images/$2/$1",
+    "~//how_to_intro_images~" => "//img/how_to/intro_images",
+    "~//imce\-uploads~" => "//embed/file",
+    "~//listing\-page\-intro\-image\-([0-9]*)\-([0-9]*)~" => "//img/listing_page/intro_images/$2/$1",
+    "~//listing_page_intro_images~" => "//img/listing_page/intro_images",
+    "~//person\-profile\-photo\-([0-9]*)\-([0-9]*)~" => "//img/person_profile/photos/$2/$1",
+    "~//photo\-image\-([0-9]*)\-([0-9]*)~" => "//img/library/photos/$2/$1",
+    "~//pictures~" => "//img/library/photos/unk",
+    "~//place\-profile\-intro\-image\-([0-9]*)\-([0-9]*)~" => "//img/place_profile/intro_images/$2/$1",
+    "~//post\-intro\-image\-([0-9]*)\-([0-9]*)~" => "//img/post/intro_images/$2/$1",
+    "~//post\-thumbnail\-([0-9]*)\-([0-9]*)~" => "//img/post/thumbnails/$2/$1",
+    "~//program\-intro\-image\-([0-9]*)\-([0-9]*)~" => "//img/program/intro_images/$2/$1",
+    "~//program\-logo\-([0-9]*)\-([0-9]*)~" => "//img/program/logo/$2/$1",
+    "~//quote\-person\-photo\-([0-9]*)\-([0-9]*)~" => "//img/quote_person/photos/$2/$1",
+    "~//tabbed\-intro\-image\-([0-9]*)\-([0-9]*)~" => "//img/tabbed/intro_images/$2/$1",
+    "~//thumbnails~" => "//img/unk/thumbnails",
+    "~//topic\-intro\-image\-([0-9]*)\-([0-9]*)~" => "//img/topic/intro_images/$2/$1",
+    "~//topic\-thumbnail\-([0-9]*)\-([0-9]*)~" => "//img/topic/thumbnails/$2/$1",
+    "~//video\-image\-([0-9]*)\-([0-9]*)~" => "//img/video/$2/$1",
   ];
 
   /**
@@ -150,10 +157,11 @@ trait FilesystemReorganizationTrait {
       }
       else {
         $count = 0;
+        // Relocate file based on regex mapping on uri.
         foreach ($this->folderMappings as $find => $replace) {
           $uri = preg_replace($find, $replace, $uri, -1, $count);
           if ($count > 0) {
-            continue;
+            break;
           }
         }
       }
