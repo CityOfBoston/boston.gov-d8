afe9be7ede36495f2c57e009ff006a7a87ff2535
David Upton <davidrkupton@gamil.com>
2019-08-22 21:39:46 -0400

    Codesniff.

--- docroot/themes/custom/bos_theme/js/messages.boston.js
+++ docroot/themes/custom/bos_theme/js/messages.boston.js
@@ -8,16 +8,13 @@
 // Adds a click event to close messages window.
 (function ($) {
   $(document).ready(function () {
-    
     $(document).ajaxComplete(function () {
       $(".message--button").on("click", function () {
         $(".bos-messages").hide();
       });
     });
-
     $(".message--button").on("click", function () {
       $(".bos-messages").hide();
     });
-
   });
 })(jQuery);
