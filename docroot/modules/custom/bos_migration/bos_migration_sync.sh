f4a9050c8f1b47c1c9fbb7000250ce2917a61f6b
David Upton <davidrkupton@gamil.com>
2019-08-26 23:19:44 -0400

    Improves daily migration script (gzip etc).

--- docroot/modules/custom/bos_migration/bos_migration_sync.sh
+++ docroot/modules/custom/bos_migration/bos_migration_sync.sh
@@ -8,18 +8,28 @@ REMOTE_SERVER="boston.prod@web-15135.prod.hosting.acquia.com"
 BACKUP_PATH="/mnt/files/boston/backups/"
 LOCAL_PATH="/mnt/files/bostond8dev/backups/on-demand/"
 
-# work out the latest backup.
+# Define the latest backup.
 FNOW=$(date +"%Y-%m-%d")
-BACKUP_FILE="prod-boston-boston-$FNOW.sql.gz"
-BACKUP="$BACKUP_PATH$BACKUP_FILE"
+BACKUP_FILE_ZIP="prod-boston-boston-$FNOW.sql.gz"
+BACKUP_FILE="prod-boston-boston-$FNOW.sql"
+REMOTE_BACKUP="$BACKUP_PATH$BACKUP_FILE_ZIP"
 
-# Copy over the latest backup.
-scp -i $KEY_FILE $REMOTE_SERVER:$BACKUP $LOCAL_PATH
+# Copy over the latest d7 prod backup.
+scp -i $KEY_FILE $REMOTE_SERVER:REMOTE_BACKUP $LOCAL_PATH
 
 # Load up the backup onto the local MySQL server.
 if [ $? -eq 0 ] && [ -f "$LOCAL_PATH$BACKUP_FILE" ]; then
+  LOCAL_BACKUP_ZIP="$LOCAL_PATH$BACKUP_FILE_ZIP"
+  LOCAL_BACKUP="$LOCAL_PATH$BACKUP_FILE"
+  # Remove existing DB.
   drush sql:drop --database=migrate -y
-  drush sql:cli --database=migrate < $LOCAL_PATH$BACKUP_FILE
+  # Unzip backup.
+  gunzip $LOCAL_BACKUP_ZIP
+  # Restore the prod backup.
+  drush sql:cli --database=migrate < $LOCAL_BACKUP
+  # Cleanup backup file.
+  rm -f $LOCAL_BACKUP
+
   # Run the migration.
   ./bos_migration reset bostond8dev
 fi
