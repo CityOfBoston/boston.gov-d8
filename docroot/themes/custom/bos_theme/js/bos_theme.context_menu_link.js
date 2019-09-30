afa6423c09aee118640a3906b91cdb2a898e61e4
David Upton <davidrkupton@gmail.com>
2019-09-29 23:01:16 -0400

    Improves the contextual link re-styling.

--- docroot/themes/custom/bos_theme/js/bos_theme.context_menu_link.js
+++ docroot/themes/custom/bos_theme/js/bos_theme.context_menu_link.js
@@ -9,49 +9,61 @@
   'use strict';
   $(document).ready(function () {
     // Var contextButtons are a list of context buttons on the page.
-    var contextButtons = $("button.trigger");
-    // Create a default type for dropdowns.
-    var type = "Element";
-    var links;
-    var entityType;
+    var rebuildPage = function () {
+      var contextButtons = $(".contextual button.trigger");
+      // Create a default type for dropdowns.
+      var type = "Element";
+      var links;
+      var entityType;
 
-    // Adds a class to the main node article element on the page.
-    $("article").first().find(".contextual").first().addClass("parent-node");
-    $('.parent-node button').addClass("large");
-    $("#main-menu").addClass("contextual-region").append($(".parent-node"));
-
-    // Adds a tooltip to the button, changes the click funtion of the contextual button when
-    // only one link and adds the node type to the links list.
-    contextButtons.each(function (key, button) {
-      links = $(button).parent().find("[class^=entitynode]");
-      if (links.length == 0) {
-        links = $(button).parent().find("[class^=paragraphs]");
-        entityType = "paragraph";
-      }
-      else {
-        entityType = "node";
-      }
-      type = $(button).parents(".contextual-region").attr("bos_context_type")
-      if (typeof type === "undefined") {
-        type = "Element";
-      }
-      type = type + " (" + entityType + ")";
-
-      if (links.length != 0) {
-        if (links.length == 1) {
-          links.parents("ul").hide();
-          links.parents(".contextual").attr({"title": "Click to Edit " + type + "."});
-          links.parent().parent().click(function () {
-            window.location.href = links.first().find("a").prop("href");
-          });
+      // Adds a tooltip to the button, changes the click funtion of the contextual button when
+      // only one link and adds the node type to the links list.
+      $(".main article.node").first().find(".contextual").first().addClass("parent-node");
+      contextButtons.each(function (key, button) {
+        links = $(button).parent().find("[class^=entitynode]");
+        if (links.length === 0) {
+          links = $(button).parent().find("[class^=paragraphs]");
+          entityType = "paragraph";
+        } else {
+          entityType = "node";
+          if ($(button).parentsUntil("article").hasClass("parent-node")) {
+            entityType = "this page";
+          }
         }
-        else if (links.length > 1) {
-          links.each(function (key, listItem) {
-            $(listItem).find("a").text($(listItem).find("a").text() + " " + type);
-          });
-          links.parents(".contextual").attr({"title": "Click for editor options (" + entityType + ")."});
+        type = $(button).parents(".contextual-region").attr("bos_context_type")
+        if (typeof type === "undefined") {
+          type = "Element";
         }
-      }
-    });
+        type = type + " (" + entityType + ")";
+
+        if (links.length !== 0) {
+          if (links.length === 1) {
+            links.parents("ul").hide();
+            links.parents(".contextual").attr({"title": "Click to Edit " + type + "."});
+            links.parent().parent().click(function () {
+              window.location.href = links.first().find("a").prop("href");
+            });
+          } else if (links.length > 1) {
+            links.each(function (key, listItem) {
+              $(listItem).find("a").text($(listItem).find("a").text() + " " + type);
+            });
+            links.parents(".contextual").attr({"title": "Click for editor options (" + entityType + ")."});
+          }
+        }
+      });
+
+      // Adds a class to the main node article element on the page.
+      $(".main article.node").first().find(".contextual").first().addClass("parent-node");
+      $('.parent-node.contextual button.trigger').addClass("large");
+      $('.parent-node .contextual-links li').each(function (lkey, listItem) {
+        var anchorText = $(listItem).find("a").text();
+        $(listItem).find("a").text(anchorText);
+      });
+      $("#main-menu").addClass("contextual-region").append($(".parent-node"));
+    };
+    window.setTimeout(function (){
+      rebuildPage();
+    }, 2000);
+
   });
 })(jQuery, this, this.document);
