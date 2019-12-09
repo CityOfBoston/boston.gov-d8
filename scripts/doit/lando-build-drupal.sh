65de4c088fa5f2edff3c72eb51d3c8385dfe5fa2
David Upton <davidrkupton@gmail.com>
2019-12-09 10:17:59 -0500

    Adds local tooling scripts to repo.

---
+++ scripts/doit/lando-build-drupal.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+  ###############################################################
+  #  These commands need to be run as normal user from lando.yml.
+  #
+  #  These commands install Drupal, sync down a database from Acquia
+  #  and update that Database with local & current repo settings.
+  ###############################################################
+
+  printf "\033[1;32m[lando]\033[1;33m Installing Drupal and dependencies in appserver & database containers ...\033[0m\n"
+
+  # Install Phing so we can use it to run balance of build scripts.
+  # Note: we want to build this from the home folder so it does not install composer.json/lock just yet.
+  printf "\033[1;32m[lando]\033[1;33m Composer is installing phing ...\033[0m\n"
+  cd ~ &&
+    composer require phing/phing:2.* --no-suggest --no-scripts -q >> ${LANDO_MOUNT}/setup/composer.log
+
+  # Capture the build info into a file to be printed at end of build process.
+  cd $LANDO_MOUNT &&
+    ~/vendor/phing/phing/bin/phing -S -f ${LANDO_MOUNT}/build.xml build:info > ${LANDO_MOUNT}/setup/uli.log
+
+  # Next command hands off to Phing to complete the initial setup.
+  printf "\033[1;32m[lando]\033[1;33m Now handing over to phing to build Drupal (\033[0;32mphing tasks annotated green\033[1;33m)...\033[0m\n"
+  printf "\033[0;32m[Build Task] setup:docker:drupal-local\033[0m\n"
+  cd $LANDO_MOUNT &&
+    ~/vendor/phing/phing/bin/phing -f ${LANDO_MOUNT}/build.xml setup:docker:drupal-local
+  printf "\033[1;32m[lando]\033[1;33m Build Finished;\033[0m\n"
+
+  # Next command creates a link to enable a cli alias for phing. (Assumes Phing is in the repo's composer.json/lock file)
+  if [ ! -e  /usr/local/bin/phing ]; then
+    ln -s ${LANDO_MOUNT}/vendor/phing/phing/bin/phing /usr/local/bin/ >> ${LANDO_MOUNT}/setup/lando.log
+  fi
+
+  # Phing commands to complete the initial setup.  Output redirect so it can be printed at the end.
+  cd $LANDO_MOUNT &&
+    phing -S -f ${LANDO_MOUNT}/build.xml update:user:setadminpwd >> ${LANDO_MOUNT}/setup/uli.log
+  cd $LANDO_MOUNT &&
+    phing -S -f ${LANDO_MOUNT}/build.xml update:user:loginadmin >> ${LANDO_MOUNT}/setup/uli.log
+  cd $LANDO_MOUNT &&
+    drush pmu acquia_connector &&
+    drush pmu config_devel &&
+    drush en config_devel >> ${LANDO_MOUNT}/setup/lando.log
+  # Now setup the node container.
+  printf "\033[1;32m[lando]\033[1;33m  Phing has finished building Drupal.\033[0m\n"
+  printf "\033[1;32m[lando]\033[1;33m Verify linux node library/extensions in node container...\033[0m"
---
+++ scripts/doit/lando-container-customize.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+  ###############################################################
+  #  These commands need to be run as root/admin user from lando.yml.
+  #
+  #  Essentially these commands are installing packages we require
+  #  in the local docker container.
+  ###############################################################
+
+    printf "\033[1;32m[lando]\033[1;33m Building; Installing Linux packages in appserver container...\033[0m"
+    if [ ! -e  /app/setup ]; then
+        mkdir /app/setup &&
+            chown www-data:www-data /app/setup &&
+            chmod 777 /app/setup;
+    fi
+    printf "\033[1;32m[lando]\033[1;33m -> Container build actions will be logged to files in ${LANDO_MOUNT}/setup\033[0m"
+
+    # Updates apt, creates and pipes output to setup/lando.log
+    mv -f /etc/apt/sources.list /etc/apt/sources.list.bak
+    sed -n '/jessie-updates/!p' /etc/apt/sources.list.bak > /etc/apt/sources.list
+    apt-get update -y  > ${LANDO_MOUNT}/setup/lando.log
+
+    # Installs linux apps and extensions into the appserver container.
+    apt-get install -y --no-install-recommends apt-utils zip unzip bzip2 libbz2-dev libgd-dev mysql-client openssh-client vim jq cron >> ${LANDO_MOUNT}/setup/lando.log
+    docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ >> ${LANDO_MOUNT}/setup/lando.log
+
+    # Change the permissions on the log file so that non-root user can add to log.
+    chmod 777 ${LANDO_MOUNT}/setup/lando.log >> ${LANDO_MOUNT}/setup/lando.log
+
+    # [Optional]  Do not need the next 2 statements on stage or production servers.
+    # Install a custom apache config file for on-demand containers -> limits the apache children to preserve memory.
+    #     (Config loaded and enabled by Phing in setup:docker:drupal-terraform)
+    cp ${LANDO_MOUNT}/scripts/phing/files/limit-apache-children.conf /etc/apache2/conf-available/ >> ${LANDO_MOUNT}/setup/lando.log
+    cp ${LANDO_MOUNT}/scripts/phing/files/boston-dev-php.ini /usr/local/etc/php/conf.d/ >> ${LANDO_MOUNT}/setup/lando.log
+
+    #- ip route | awk 'NR==1 {printf $3}' | xargs echo "xdebug.remote_host=" >>/usr/local/etc/php/conf.d/boston-dev-php.ini
+    service apache2 reload >> ${LANDO_MOUNT}/setup/lando.log
