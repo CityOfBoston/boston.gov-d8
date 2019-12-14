9a347ddf81ddd65eca2c9912d12f29f349456956
David Upton <davidrkupton@gmail.com>
2019-12-13 23:06:32 -0500

    Remove phing WIP

--- scripts/local/lando_utilities.sh
+++ scripts/local/lando_utilities.sh
@@ -132,8 +132,8 @@ function build_settings() {
         echo -e "}\n\n" >> ${local_settings_file}
     fi
     # Add in config sync directory from yml.
-    echo -e "\$config_directories['sync'] = '${build_local_config_sync}';\n" >> ${local_settings_file}
-    echo -e "\$settings['install_profile'] = '${project_profile_name}';\n" >> ${local_settings_file}
+    echo -e "\$config_directories[\"sync\"] = \"${build_local_config_sync}\";\n" >> ${local_settings_file}
+    echo -e "\$settings[\"install_profile\"] = \"${project_profile_name}\";\n" >> ${local_settings_file}
     echo -e "/* End of additions. */\n" >> ${local_settings_file}
 
     # setup the private settings file
