836e9ce0d6f4ef5fdc23e9184f1f05a30696e6bb
David Upton <davidrkupton@gmail.com>
2019-12-24 23:27:15 -0500

    Proper track-branch setup

---
+++ scripts/local/lando-post-start.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+###############################################################
+#  These commands need to be run as normal user from lando.yml.
+#
+#  NOTE: THIS SCRIPT SHOULD BE RUN INSIDE THE CONTAINER.
+#
+#  These commands install Drupal, sync down a database from Acquia
+#  and update that Database with local & current repo settings.
+#
+#  PRE-REQUISITES:
+#     - docker container for appserver is created and started, and
+#     - main boston.gov repo is already cloned onto the host machine, and
+#     - .lando.yml and .config.yml files are correctly configured.
+#
+#  Basic workflow:
+#     1. Use composer to gather all Drupal core and contributed modules.
+#     2. Clone the private repo and merge into the main repo.
+#     3. Prepare/update settings.php and other settings files
+#     4. Create the Drupal MySQL Database (initialize new or sync existing from remote)
+#     5. Import configuration from main repo (already cloned locally)
+#     6. Ensure the Drupal site is configured properly for develop activities
+#     7. Run any finalization tasks to complete.
+###############################################################
+
+    # Include the utilities file/libraries.
+    # This causes the .lando.yml and .config.yml files to be read in and stored as variables.
+    REPO_ROOT="${LANDO_MOUNT}"
+    . "${LANDO_MOUNT}/scripts/cob_build_utilities.sh"
+    . "${LANDO_MOUNT}/hooks/common/cob_utilities.sh"
+
+    printout "LANDO" "Project Event - post-start\n"
+    printf "================================================================================\n"
+    printout "SUCCESS" "Appserver and MySQL Docker containers are now started."
+    printf "================================================================================\n\n"
+
+    if [[ -e ${REPO_ROOT}/setup/uli.log ]]; then
+        cat ${REPO_ROOT}/setup/uli.log && rm -f ${REPO_ROOT}/setup/uli.log;
+    else
+        . ${REPO_ROOT}/scripts/doit/branding.sh;
+    fi
--- scripts/local/test.sh
+++ scripts/local/test.sh
@@ -6,19 +6,12 @@
 
   eval $(parse_yaml "${LANDO_MOUNT}/scripts/.config.yml" "")
   eval $(parse_yaml "${LANDO_MOUNT}/.lando.yml" "lando_")
-TRAVIS_BRANCH="replace-phi_ng"
-TRAVIS_BRANCH=${TRAVIS_BRANCH/-/}
-TRAVIS_BRANCH=${TRAVIS_BRANCH/_/}
-TRAVIS_BRANCH=${TRAVIS_BRANCH/ /}
-    # Define branch-specific variables.
-    src="deploy_${TRAVIS_BRANCH}_dir" && echo ${src} && deploy_dir="${!src}"
-    printf "$deploy_dir\n${TRAVIS_BRANCH}"
 
 #  ( set -o posix ; set )
-#  printout "STEP" "This is step" "here"
-#  printout "SUCCESS" "This is success" "here"
-#  printout "ERROR" "This is error" "here"
-#  printout "WARNING" "This is warning" "here"
-#  printout "INFO" "This is info" "here"
-#  printout "UNK" "This is unknown" "here"
+  printout "STEP" "This is step" "here"
+  printout "SUCCESS" "This is success" "here"
+  printout "ERROR" "This is error" "here"
+  printout "WARNING" "This is warning" "here"
+  printout "INFO" "This is info" "here"
+  printout "UNK" "This is unknown" "here"
 
