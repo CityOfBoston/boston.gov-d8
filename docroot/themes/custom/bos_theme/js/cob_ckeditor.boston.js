1583a9b81a16c53d2a111b385a47a2e057d47369
stella.ubaha@boston.gov <stella.ubaha@boston.gov>
2020-01-13 14:05:01 -0500

    codesniff!!!

--- docroot/themes/custom/bos_theme/js/cob_ckeditor.boston.js
+++ docroot/themes/custom/bos_theme/js/cob_ckeditor.boston.js
@@ -11,20 +11,20 @@ const x = window.matchMedia("(max-width: 1100px)");
 function screenSize(x) {
   let changeHeight = document.querySelectorAll(".cob-ckeditor .cob-ckeditor-bg");
 
-  if (x.matches) { // If media query matches
+  if (x.matches) { // If media query matches.
 
     document.body.style.backgroundColor = "yellow";
-    for( let i=0; i<changeHeight.length; i++) {
+    for (let i = 0; i < changeHeight.length; i++) {
       let show = changeHeight[i].getAttribute("data-cob-ckeditor");
       changeHeight[i].style.height = show - 100 + "px";
       changeHeight[i].style.minHeight = show - 100 + "px";
-      //console.log(changeHeight[i].style.height = show - 100);
     }
 
-  } else {
+  }
+  else {
     x = window.matchMedia("(min-width: 1101px)");
     if (x.matches) {
-      for( let i=0; i<changeHeight.length; i++) {
+      for (let i = 0; i < changeHeight.length; i++) {
         let show = changeHeight[i].getAttribute("data-cob-ckeditor");
         changeHeight[i].style.height = show + "px";
         changeHeight[i].style.minHeight = show + "px";
@@ -33,9 +33,7 @@ function screenSize(x) {
   }
 }
 
-//screenSize(x); // Call listener function at run time
+// Call listener function at run time.
 x.addEventListener("change", () => {
   screenSize(x);
 });
-
-
