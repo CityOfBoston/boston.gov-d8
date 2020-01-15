1fdaebf7943f1826747d3b49d2267010d750c25e
Stella Ubaha <57374913+subaha-cob@users.noreply.github.com>
2020-01-15 15:19:28 -0500

    Deployed to Stage (#1186)
    * Issue 1163 1162 908 (#1182)
    
    * Fix for issue #1163 #1162, #908
    
    * Fix for issue #1163 #1162, #908, procurement config files
    
    * Fix for issue #1163 #1162, #908, procurement config files
    
    * codesniff!!!
    
    * Updates calendar view to use recurring dates for events.
    
    * Codesniff !!
    
    Co-authored-by: David Upton <davidrkupton@users.noreply.github.com>
    Co-authored-by: Phillip B. Kelly <phillipbentonkelly@gmail.com>

---
+++ docroot/themes/custom/bos_theme/js/cob_ckeditor.boston.js
@@ -0,0 +1,39 @@
+/**
+ * @file
+ * CKeditor  Functionality.
+ *
+ * Changes the height of an image via media query API.
+ */
+
+'use strict';
+
+const x = window.matchMedia("(max-width: 1100px)");
+function screenSize(x) {
+  let changeHeight = document.querySelectorAll(".cob-ckeditor .cob-ckeditor-bg");
+
+  if (x.matches) { // If media query matches.
+
+    document.body.style.backgroundColor = "yellow";
+    for (let i = 0; i < changeHeight.length; i++) {
+      let show = changeHeight[i].getAttribute("data-cob-ckeditor");
+      changeHeight[i].style.height = show - 100 + "px";
+      changeHeight[i].style.minHeight = show - 100 + "px";
+    }
+
+  }
+  else {
+    x = window.matchMedia("(min-width: 1101px)");
+    if (x.matches) {
+      for (let i = 0; i < changeHeight.length; i++) {
+        let show = changeHeight[i].getAttribute("data-cob-ckeditor");
+        changeHeight[i].style.height = show + "px";
+        changeHeight[i].style.minHeight = show + "px";
+      }
+    }
+  }
+}
+
+// Call listener function at run time.
+x.addEventListener("change", () => {
+  screenSize(x);
+});
