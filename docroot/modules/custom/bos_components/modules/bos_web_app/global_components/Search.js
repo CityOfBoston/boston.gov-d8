3d0823f3711517b9ad13f37cdf4b23ec356c2cff
Matthew McGowan <matthew.mcgowan@boston.gov>
2020-02-13 13:11:04 -0500

    Updated Search box styles.

--- docroot/modules/custom/bos_components/modules/bos_web_app/global_components/Search.js
+++ docroot/modules/custom/bos_components/modules/bos_web_app/global_components/Search.js
@@ -4,14 +4,14 @@ class Search extends React.Component {
   render() {
       return (
         <div className="sf">
-          <div className="sf-i">
+          <div className="sf-i mnl-search">
             <input
                 type="text"
                 value={this.props.currentKeywords}
                 style={this.props.styleInline}
                 aria-label={this.props.placeholder}
                 placeholder={this.props.placeholder}
-                className={this.props.searchClass}
+                className={this.props.searchClass + ' search'}
                 onChange={this.props.handleKeywordChange}
                 onKeyUp={this.props.handleKeywordChange}
             />
