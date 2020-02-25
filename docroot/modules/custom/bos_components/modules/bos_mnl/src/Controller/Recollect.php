318dabd4314a48bcd5316b323fd6ca4ace492c20
Matthew McGowan <44584567+mmcgowanBOS@users.noreply.github.com>
2020-02-24 23:08:58 -0500

    Linting again.

--- docroot/modules/custom/bos_components/modules/bos_mnl/src/Controller/Recollect.php
+++ docroot/modules/custom/bos_components/modules/bos_mnl/src/Controller/Recollect.php
@@ -74,7 +74,7 @@ class Recollect extends ControllerBase {
     $ch = curl_init();
     $address_formatted = preg_replace('/[[:space:]]+/', '+', $addressData);
     $place = $this->getPlaceId($address_formatted);
-    curl_setopt($ch, CURLOPT_URL,"https://api.us.recollect.net/v2/places/" . $place . "/services/waste");
+    curl_setopt($ch, CURLOPT_URL, "https://api.us.recollect.net/v2/places/" . $place . "/services/waste");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
     curl_setopt($ch, CURLOPT_HEADER, FALSE);
     curl_setopt($ch, CURLOPT_HTTPHEADER, [
