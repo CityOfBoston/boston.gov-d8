e214b545ae4dae6a5a134dc836b720807d53a9ca
David Upton <davidrkupton@gamil.com>
2020-03-14 20:54:15 -0400

    Updates tooling.

--- scripts/local/pull.sh
+++ scripts/local/pull.sh
@@ -3,7 +3,11 @@
     printout "INFO" "Will pull latest code from public repo and merge in settings from private repo."
 
     # Include the utilities file/library.
+    if [[ -e "${drush_cmd}" ]]; then
+        drush_cmd="/app/vendor/bin/drush  -r /app/docroot"
+    fi
     . "/app/scripts/cob_build_utilities.sh"
+    . "/app/hooks/common/cob_utilities.sh"
 
     # Pull develop branch from git.
     cd /app/docroot &&
