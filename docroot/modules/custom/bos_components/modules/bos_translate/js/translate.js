ca98836ad87aecdd1cb2797de4200b7dd5dd6186
stella.ubaha@boston.gov <stella.ubaha@boston.gov>
2020-04-27 18:51:36 -0400

    Translate API Update to UAT

--- docroot/modules/custom/bos_components/modules/bos_translate/js/translate.js
+++ docroot/modules/custom/bos_components/modules/bos_translate/js/translate.js
@@ -10,13 +10,19 @@ jQuery(document).ready( function () {
     e.preventDefault();
     jQuery('.translate-dd').toggle('slow');
   });
+
+  GoogleTranslateLink();
+
 });
 
 function GoogleTranslateLink() {
   if (window.location.href.indexOf("translate.google.com") == -1) {
-    var url = "https://translate.google.com/translate?js=y&prev=_t&hl=en&ie=UTF-8&layout=1&eotf=1&sl=en&tl=es&u=";
-    url += escape(window.location.href);
-    window.location.href = url;
+    jQuery('.translate-dd-link').click( function() {
+      let dataLang = jQuery(this).attr('data-lang');
+      let url = "https://translate.google.com/translate?js=y&prev=_t&hl=en&ie=UTF-8&layout=1&eotf=1&sl=en&tl=" + dataLang + "&u=";
+      url += escape(window.location.href);
+      window.location.href = url;
+    });
   }
   else {
     //do nothing
