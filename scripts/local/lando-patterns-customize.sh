3af54ea906ef067b82cca82834d874f6e25798c3
David Upton <davidrkupton@gmail.com>
2020-04-27 20:28:05 -0400

    Ensures drupal switches to local mode when available.

--- scripts/local/lando-patterns-customize.sh
+++ scripts/local/lando-patterns-customize.sh
@@ -16,7 +16,7 @@
 
     printf "\n"
     printf "ref: $(basename "$0")\n"
-    if [[ "${patterns_local_repo_local_dir}" != "true" ]] && [[ "${patterns_local_repo_local_dir}" != "True" ]] && [[ "${patterns_local_repo_local_dir}" != "TRUE" ]]; then
+    if [[ "${patterns_local_build}" != "true" ]] && [[ "${patterns_local_build}" != "True" ]] && [[ "${patterns_local_build}" != "TRUE" ]]; then
         printout "INFO" "Patterns library will not be deployed."
         exit 0
     fi
@@ -28,3 +28,11 @@
     if [[ -d ${patterns_local_repo_local_dir} ]]; then rm -rf ${patterns_local_repo_local_dir}; fi
     mkdir ${patterns_local_repo_local_dir}
     chown node:node ${patterns_local_repo_local_dir}
+
+    # Copy the node.js executable file in the container to a location that can be seen on the host.
+    # This way eslint can run from PHPStorm without needing to install node.js on the host.
+    # In PHPStorm point the path for node in eslint settings dialog to /user/.node_js/node
+    if [[ "${patterns_local_build}" != "true" ]] && [[ "${patterns_local_build}" != "True" ]] && [[ "${patterns_local_build}" != "TRUE" ]]; then
+        if [[ ! -e /user/.node_js ]]; then mkdir /user/.node_js; fi
+        cp /usr/local/bin/node /user/.node_js/.
+    fi
