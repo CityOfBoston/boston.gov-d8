37063c4a2c5a18b88780d484725a6b1ec1317844
David Upton <davidrkupton@gmail.com>
2020-05-08 18:35:37 -0400

    Updates logging and messages.

--- scripts/local/pull.sh
+++ scripts/local/pull.sh
@@ -8,15 +8,19 @@
     . "/app/scripts/cob_build_utilities.sh"
     . "/app/hooks/common/cob_utilities.sh"
 
+    target_env="local"
+
     printout "INFO" "Will pull latest code from public repo and merge in settings from private repo."
 
     # Pull current boston.gov branch from git.
+    printout "INFO" "Pulling current branch from Boston.gov-d8."
     cd /app/docroot &&
         git fetch --all >> /dev/null &&
         git pull --all >> /dev/null
 
     # Clone the private repo and merge with the main repo.
     clone_private_repo
+    build_settings
 
     # Check if the patterns repo folder exists, if not, then have to clone repo.
     if [[ ! -d ${patterns_local_repo_local_dir} ]]; then
@@ -24,14 +28,19 @@
         clone_patterns_repo
     fi
     # Pull patterns current branch from git.
+    printout "INFO" "Pulling current branch from Patterns."
     cd /app/patterns &&
         git fetch --all >> /dev/null &&
         git pull --all >> /dev/null
 
     # Check for options/flags passed in.
     if [[ "${1}" != "--no-sync" ]]; then
+        printout "INFO" "Preparing Config Import"
+        printf "       - Don't worry that modules are uninstalled here - they will be re-enabled later."
         ${drush_cmd} cim -y
         ${drush_cmd} updb -y
+        printout "INFO" "Resetting modules for development."
+        devModules
     fi
 
     printout "SUCCESS" "Boston.gov, Patterns and Private repos updated.\n"
