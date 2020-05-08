407e12d2da76bc88f95fa9fdd2eff4978e0e8786
David Upton <davidrkupton@gmail.com>
2020-05-08 17:07:31 -0400

    Updates lando tooling to include cloning patterns repos.

--- scripts/local/lando-build-patterns.sh
+++ scripts/local/lando-build-patterns.sh
@@ -37,32 +37,7 @@
     printf "${LightPurple}       ================================================================================${NC}\n"
 
     # Clone the patterns repo into a folder within the Main boston.gov d8 repo.
-    if [[ -n ${GITHUB_TOKEN} ]]; then
-        # Will enforce a token which should be passed via and ENVAR.
-        REPO_LOCATION="https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/"
-    else
-        # Will rely on the user have an SSL cert which is registered with the private repo.
-        REPO_LOCATION="git@github.com:"
-    fi
-    printout "INFO" "Cloning ${patterns_local_repo_branch} branch of Patterns library."
-    # Create a clean folder into which the repo can be cloned.
-    if [[ ! -d ${patterns_local_repo_local_dir} ]]; then
-        mkdir ${patterns_local_repo_local_dir}
-        chown node:node ${patterns_local_repo_local_dir}
-    fi
-    git clone -b ${patterns_local_repo_branch} ${REPO_LOCATION}${patterns_local_repo_name} ${patterns_local_repo_local_dir} -q --depth 100
-    if [[ $? != 0 ]]; then
-        printout "ERROR" "Patterns library NOT cloned or installed."
-        exit 1
-    fi
-    # Make the public folder that gulp and fractal will build into.
-    if [[ ! -d ${patterns_local_repo_local_dir}/public ]]; then
-        mkdir ${patterns_local_repo_local_dir}/public
-        chown node:node ${patterns_local_repo_local_dir}/public
-        chmod 755 ${patterns_local_repo_local_dir}/public
-    fi
-    printout "SUCCESS" "Patterns library cloned."
-
+    clone_patterns_repo
 
     # Install the patterns app.
     printout "INFO" "Building Patterns library."
