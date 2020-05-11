09d928d878817bdc0d5bb55d6ee6031f7434e303
David Upton <davidrkupton@gmail.com>
2020-05-11 19:14:57 -0400

    Update to node:10 in patterns container.

--- scripts/local/lando-build-patterns.sh
+++ scripts/local/lando-build-patterns.sh
@@ -23,9 +23,10 @@
     REPO_ROOT="${LANDO_MOUNT}"
     . "${LANDO_MOUNT}/scripts/cob_build_utilities.sh"
     . "${LANDO_MOUNT}/hooks/common/cob_utilities.sh"
+    target_env="local"
 
     printf "\n"
-    printf "ref: $(basename "$0")\n"
+    printf "[LANDO] starts <$(basename "$0")>\n"
     if [[ "${patterns_local_build}" != "true" ]] && [[ "${patterns_local_build}" != "True" ]] && [[ "${patterns_local_build}" != "TRUE" ]]; then
         printout "INFO" "Patterns library will not be deployed.."
         exit 0
@@ -34,19 +35,17 @@
     printout "STEP" "Building Patterns."
     printf "${LightPurple}       ================================================================================${NC}\n"
 
-    # Clone the patterns repo into a folder within the Main boston.gov d8 repo.
-    clone_patterns_repo
-
-    # Install the patterns app.
-    printout "INFO" "Building Patterns library."
-    cd ${patterns_local_repo_local_dir} && npm install
+    printout "INFO" "Installing node dependencies for patterns app."
+    cd ${patterns_local_repo_local_dir} && npm run preinstall && npm install && npm install -g gulp-cli@latest
     if [[ $? != 0 ]]; then
         printout "ERROR" "Patterns library NOT built or installed."
         exit 1
     fi
 
+    # Install the patterns app.
+    printout "INFO" "Building Patterns library."
     # Run an initial build to be sure everything is there.
-    printout "INFO" "Build Stuff."
-    cd ${patterns_local_repo_local_dir} && npm run preinstall && npm run fractal-build && npm run gulp-build
+    cd ${patterns_local_repo_local_dir} && npm run build
 
     printout "SUCCESS" "Patterns library built."
+    printf "[LANDO] ends <$(basename "$0")>\n"
