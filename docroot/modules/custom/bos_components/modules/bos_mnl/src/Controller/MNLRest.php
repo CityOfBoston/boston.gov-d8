b5e2cf5e873796060422cda329971b985fba1a1d
David Upton <davidrkupton@gmail.com>
2020-05-16 14:29:42 -0400

    Improves logging and adds documentation.

--- docroot/modules/custom/bos_components/modules/bos_mnl/src/Controller/MNLRest.php
+++ docroot/modules/custom/bos_components/modules/bos_mnl/src/Controller/MNLRest.php
@@ -10,6 +10,59 @@ use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * MNLRest class for endpoint.
+ *
+ * All POSTS to endpoint must have format:
+ *   /rest/mnl/YYY?api_key=XXX
+ *   Where YYYY is one of 'update', 'import' or 'manual'.
+ *   Where api_key XXX is the token defined at /admin/config/services/mnl.
+ *
+ * Note: 'import' and 'update' endpoint usage must include a json string in the
+ *   body in the format:
+ *   [
+ *     {"sam_address_id":123,"full_address":"address","data": jsonstring"},
+ *     {"sam_address_id":123,"full_address":"address","data": jsonstring"}
+ *   ]
+ *   The 'jsonstring' must be a valid json object in string form and can contain
+ *   any data provided it represents a single record and can be decoded by
+ *   PHP's json_decode function.
+ *
+ * Note: provide a querystring for "manual" endpoint in format:
+ *   ?api_key=XXXXX&path=/path-on-server/file.json&limit=N&mode=import
+ *   Required:
+ *     'path' is path on remote server (or local container) to import file.
+ *   Optional:
+ *     'limit' process the first N records in the import file (defaults all).
+ *     'mode' can be either 'import' or 'update' (defaults to 'update').
+ *
+ * Note:
+ *  - UPDATE updates "full address" and "data" for SAM records in DB with
+ *    "full address" and "data" from records with a matchin "sam_address_id"
+ *    from the json payload. New records will be created if an existing
+ *    "sam_address_id" is not found in the DB. If there are duplicate
+ *    "sam_address_id"s in the import, only the first of the duplicate records
+ *    will be processed. No records will be deleted or marked for deletion.
+ *  - IMPORT also updates "full address" and "data" for SAM records in DB with
+ *    "full address" and "data" from records with a matchin "sam_address_id"
+ *    from the json payload. New records will also be created if an existing
+ *    "sam_address_id" is not found in the DB. If there are duplicate
+ *    "sam_address_id"s in the import, only the first of the duplicate records
+ *    will be processed.  If the database contains a record (ie. a
+ *    "sam_address_id") which is not in the import file, then after the import
+ *    completes, those "orphaned" records will be cleaned, by removing them
+ *    from the database.
+ *
+ * Note: Actual updating and deleting (when done) will be handled by queues that
+ *   are processed overnight by a scheduled task, so the endpoint only queues
+ *   tasks for later execution, it does not actually change data itself.
+ *
+ * Benchmarks 16 May 2020:
+ *   - Acquia/AWS Uploaded 1.4GB file with 400,000 records in 12 secs.
+ *   - Drupal: 400,000 records queued in less than 00:03:30 (3.5 mins).
+ *   - mnl_import queue processes (local) so acquia will be 5-10x faster:
+ *        - processed 395,037 records with 5,000 updates in 10 mins
+ *        - processed 395,037 records with 395,037 updates in 15 mins
+ *        - processed 395,037 records with 395,037 inserts (new nodes) - 58 min
+ *        - processed 50 deletes in 10sec
  */
 class MNLRest extends ControllerBase {
 
@@ -68,10 +121,10 @@ class MNLRest extends ControllerBase {
       $data = json_decode(strip_tags($data), TRUE);
     }
     else {
-      \Drupal::queue('mnl_import')->deleteQueue();
       \Drupal::queue('mnl_cleanup')->deleteQueue();
       $path = \Drupal::request()->get("path");
       $limit = \Drupal::request()->get("limit");
+      $operation = (\Drupal::request()->get("mode") ?: "update");
       if (NULL != $path && file_exists($path)) {
         $data = file_get_contents($path);
         $data = json_decode($data);
@@ -111,15 +164,13 @@ class MNLRest extends ControllerBase {
       }
 
       elseif ($operation == "import") {
-        $queue = \Drupal::queue('mnl_import');
-      }
-
-      elseif ($operation == "manual") {
-        $queue = \Drupal::queue('mnl_import');
+        $queue_name = 'mnl_import';
+        $queue = \Drupal::queue($queue_name);
       }
 
       elseif ($operation == "update") {
-        $queue = \Drupal::queue('mnl_update');
+        $queue_name = 'mnl_update';
+        $queue = \Drupal::queue($queue_name);
       }
 
       else {
@@ -136,9 +187,7 @@ class MNLRest extends ControllerBase {
           $queue->createItem($items);
         }
         \Drupal::logger("mnl import")
-          ->info("[0] REST $operation Import loaded " . count($data) . " SAM records into mnl_import queue.");
-        \Drupal::logger("mnl import")
-          ->info("[0] mnl_import queue now contains " . $queue->numberOfItems() . " SAM records to be processed.");
+          ->info("[0] REST payload contained " . number_format(count($data), 0) . " SAM records. <br>Loaded " . number_format($queue->numberOfItems()) . " records with unique SAM ID's into queue $queue_name");
 
         $response_array = [
           'status' => $operation . ' complete - ' . count($data) . ' items queued',
