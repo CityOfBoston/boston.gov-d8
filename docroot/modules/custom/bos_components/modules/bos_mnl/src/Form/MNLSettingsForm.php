d1c7767c0cecf686127bac737dd405e6ca1deb51
David Upton <davidrkupton@gmail.com>
2020-05-16 15:31:12 -0400

    Adds documentation to the admin page.

--- docroot/modules/custom/bos_components/modules/bos_mnl/src/Form/MNLSettingsForm.php
+++ docroot/modules/custom/bos_components/modules/bos_mnl/src/Form/MNLSettingsForm.php
@@ -33,6 +33,9 @@ class MNLSettingsForm extends ConfigFormBase {
     $config = $this->config('bos_mnl.settings');
     $form = [
       '#tree' => TRUE,
+      'label1' => [
+        "#markup" => "<h3>This module exposes REST endpoints at <b><i>/rest/mnl/xxxx</i>.</b></h3><p>See documentation below for REST endpoint syntax.</p>"
+      ],
       'mnl_admin' => [
         '#type' => 'fieldset',
         '#title' => 'MNL API Endpoint',
@@ -44,6 +47,52 @@ class MNLSettingsForm extends ConfigFormBase {
           '#default_value' => $config->get('auth_token'),
           '#required' => FALSE,
         ],
+      ],
+      'label2' => [
+        "#markup" => "<h3>Notes:</h3><ol><li>The REST endpoint merely loads data into a queue.</li>
+        <li>To perform and automate the update of MNL/SAM entities/nodes for use by the MNL module, 2 scheduled tasks must be created.<br><ul><li>The first task should execute the following command:<pre>drush queue:run mnl_import &> /dev/null && drush queue:run mnl_cleanup &> /dev/null</pre></li><li>and the second task should execute<pre>drush queue:run mnl_update &> /dev/null</pre></li></ul>Timing for scheduling those tasks should be some time after the expected completion of data submission via the REST endpoint.</li></ol>"
+      ],
+      'label3' => [
+        "#markup" => "<h4>If the scheduled tasks above are not created then the website will not be updated until the commands above are run manually.</h4>"
+      ],
+      'label4' => [
+        "#markup" => "<br><hr><h3>Documentation:</h3>
+All POSTS to endpoint must have format:
+  <ul><pre>/rest/mnl/YYY?api_key=XXX</pre></ul>
+  <ul><li>Where YYYY is one of 'update', 'import' or 'manual'.</li>
+  <li>Where api_key XXX is the <i><b>API KEY / token</b></i> defined above.</li></ul><br>
+'import' and 'update' endpoint usage must include a json string in the
+  body in the format:
+<ul><pre>[
+    {\"sam_address_id\":123,\"full_address\":\"address\",\"data\":jsonstring\"},
+    {\"sam_address_id\":123,\"full_address\":\"address\",\"data\":jsonstring\"}
+]</pre></ul>
+  <ul><li>The 'jsonstring' must be a valid json object in string form and can contain
+  any data provided it represents a single record and can be decoded by
+  PHP's json_decode function.</li></ul><br>
+'manual' endpoint requires no body (any body will be ignored) but must have an extended querystring in the format:
+  <ul><pre>?api_key=XXX&path=/path-on-server/file.json&limit=N&mode=ZZZ</pre></ul>
+  <i>Required:</i>
+    <ul><li>'path' is path on remote server (or local container) to import file.</li></ul>
+  <i>Optional:</i>
+    <ul><li>'limit' process the first N records in the import file (defaults all).</li>
+    <li>'mode' ZZZ can be either 'import' or 'update' (defaults to 'update').</li></ul>
+<b>Note:</b>
+ <ul><li><b>UPDATE</b> updates \"full address\" and \"data\" of SAM records in the DB with
+   \"full address\" and \"data\" from queued records with a matching \"sam_address_id\"
+   from the json payload. New records will be created if an existing
+   \"sam_address_id\" is not found in the DB. If there are duplicate
+   \"sam_address_id\"s in the json payload, only the first one of the duplicate records
+   will be processed and the rest will be skipped. No records will be deleted or marked for deletion.</li>
+ <li><b>IMPORT</b> also updates \"full address\" and \"data\" of SAM records in the DB with
+   \"full address\" and \"data\" from queued records with a matching \"sam_address_id\"
+   from the json payload. New records will also be created if an existing
+   \"sam_address_id\" is not found in the DB. If there are duplicate
+   \"sam_address_id\"s in the json payload, only the first one of the duplicate records
+   will be processed and the rest will be skipped.  If the database contains a record (ie. a
+   \"sam_address_id\") which is not in the import file, then after the import
+   completes, those \"orphaned\" records will be cleaned, by removing them
+   from the database.</li></ul>"
       ],
     ];
     return parent::buildForm($form, $form_state);
