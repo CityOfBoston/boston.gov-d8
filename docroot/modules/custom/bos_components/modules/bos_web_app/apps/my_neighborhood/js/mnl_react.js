02e213361735415e222f8911d901792aef8de62f
Matthew McGowan <matthew.mcgowan@boston.gov>
2020-07-16 21:25:56 -0400

    Added cross origin stuff.

--- docroot/modules/custom/bos_components/modules/bos_web_app/apps/my_neighborhood/js/mnl_react.js
+++ docroot/modules/custom/bos_components/modules/bos_web_app/apps/my_neighborhood/js/mnl_react.js
@@ -177,9 +177,11 @@ class MNL extends React.Component {
         "&sort=" +
         paramsQuery.sort,
         {
-          "mode": "no-cors",
+          "mode": "cors",
           "headers": {
             "Content-Type": "application/json",
+            // Needed for CORS google translate
+            "Access-Control-Allow-Origin": "https://translate.googleusercontent.com"
           },
         },
         
