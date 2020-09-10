714e3797ee81457c42e4ed938892d1f98a00aab2
David Upton <davidrkupton@gmail.com>
2020-09-10 19:11:40 -0400

    Improves logging during health checks

--- scripts/local/health.sh
+++ scripts/local/health.sh
@@ -11,13 +11,15 @@ if [[ -n "$command" ]]; then
         if [[ -d /app/patterns/public/css ]]; then
             # If the patterns folder is populated, then try to get the fractal server
             wget http://127.0.0.1 -O .null0.test || exit 1
+            rm -f .null0.test
             exit 0
         else
             # Nothing is loaded yet, so just report all is good.
             exit 0
         fi
     elif [[ "${command}" == "appserver" ]]; then
-         mysql -hdatabase -P3306 -udrupal --password=drupal -e"show databases;" && wget http://boston.lndo.site -O .null1.test || exit 1
+        mysql -hdatabase -P3306 -udrupal --password=drupal -e"show databases;" && wget http://boston.lndo.site -O .null1.test || exit 1
+        rm -f .null1.test
         exit 0
     fi
 fi
