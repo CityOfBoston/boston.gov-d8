227ab9c773c093bd353b839f30f011854b60a027
David Upton <davidrkupton@gmail.com>
2020-09-10 16:02:19 -0400

    Adds scripts for the node container.

---
+++ scripts/local/lando-build-node.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+###############################################################
+#  These commands need to be run as normal user from lando.yml.
+#
+#  NOTE: THIS SCRIPT SHOULD BE RUN INSIDE THE CONTAINER.
+#
+#  These commands install the patterns repo, start the node service
+#  and start gulp to monitor for changes to files.
+#
+#  PRE-REQUISITES:
+#     - docker container for node is created and started, and
+#     - .lando.yml and .config.yml files are correctly configured.
+#
+#  Basic workflow:
+
+#   1. Clone the patterns repo
+#   2. Install npm dependencies in package.json
+###############################################################
+
+  printf "\n"
+  printf "[LANDO] starts <$(basename "$0")>\n"
+
+  . "${LANDO_MOUNT}/scripts/lando/lando-build-patterns.sh"
+  . "${LANDO_MOUNT}/scripts/lando/lando-build-webapps.sh"
+
+  printf "[LANDO] ends <$(basename "$0")>\n"
---
+++ scripts/local/lando-node-customize.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+  ###############################################################
+  #  These commands need to be run as root/admin user from lando.yml.
+  #
+  #  Essentially these commands are installing packages we require
+  #  in the local docker node container.
+  ###############################################################
+
+  printf "\n"
+  printf "[LANDO] starts <$(basename "$0")>\n"
+
+  # Include the utilities file/library.
+  # This causes the .lando.yml and .config.yml files to be read in and stored as variables.
+  . "${LANDO_MOUNT}/scripts/lando/lando-patterns-customize.sh"
+  . "${LANDO_MOUNT}/scripts/lando/lando-webapps-customize.sh"
+
+  printf "[LANDO] ends <$(basename "$0")>\n"
