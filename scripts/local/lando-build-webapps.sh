916d435f12e92e213b984798b96876258b9af9d6
David Upton <davidrkupton@gmail.com>
2020-09-10 16:03:34 -0400

    Adds scripts for managing webapps inside the node container.

---
+++ scripts/local/lando-build-webapps.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+###############################################################
+#  These commands need to be run as normal user from lando.yml.
+#
+#  NOTE: THIS SCRIPT SHOULD BE RUN INSIDE THE CONTAINER.
+#
+#  PRE-REQUISITES:
+#     - docker container for node is created and started, and
+#     - .lando.yml and .config.yml files are correctly configured.
+#
+###############################################################
+    REPO_ROOT="${LANDO_MOUNT}"
+    . "${LANDO_MOUNT}/scripts/cob_build_utilities.sh"
+    target_env="local"
+
+    printf "\n"
+    printf "[LANDO] starts <$(basename "$0")>\n"
+
+    # Global Dependencies.
+    # Install a standard common set of dependencies required for the local build.
+    # (This is all dependencies from the package.json file in the scripts folder).
+    cd ${REPO_ROOT}/scripts && npm install
+
+    printf "[LANDO] ends <$(basename "$0")>\n"
---
+++ scripts/local/lando-webapps-post-start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+    ###############################################################
+    #  These commands need to be run as normal user from lando.yml.
+    #
+    #  NOTE: THIS SCRIPT SHOULD BE RUN INSIDE THE CONTAINER.
+    #
+    #  These commands start the webapp processes
+    #
+    ###############################################################
+
+    REPO_ROOT="${LANDO_MOUNT}"
+    . "${LANDO_MOUNT}/scripts/cob_build_utilities.sh"
+    target_env="local"
+
+    printf "\n"
+    printf "[LANDO] starts <$(basename "$0")>\n"
+
+    # Here we need to search for webapps, detect and start their key watch processes
+
+    printf "[LANDO] ends <$(basename "$0")>\n"
