4e27043c1ea4068c7192e8da304a12a04bc5a47c
Matthew McGowan <matthew.mcgowan@boston.gov>
2020-10-16 02:05:56 -0400

    Added route, controller, and endpoint for Upaknee subscribers.

---
+++ docroot/modules/custom/bos_components/modules/bos_email/src/Controller/UpakneeAPI.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace Drupal\bos_email\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Cache\CacheableJsonResponse;
+use Drupal\Core\Site\Settings;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Upaknee class for API.
+ */
+class UpakneeAPI extends ControllerBase {
+
+  /**
+   * Current request object for this class.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  public $request;
+
+  /**
+   * Operation mapped to Upaknee.
+   *
+   * @var string
+   */
+  public $operation;
+
+  /**
+   * Public construct for Request.
+   */
+  public function __construct(RequestStack $request) {
+    $this->request = $request;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    // Instantiates this form class.
+    return new static(
+      // Load the service required to construct this class.
+      $container->get('request_stack')
+    );
+  }
+
+  /**
+   * Get ENV var token for Upaknee auth.
+   */
+  public function getToken() {
+    $token = NULL;
+
+    if (isset($_ENV['UPAKNEE_TOKEN'])) {
+      $token = $_ENV['UPAKNEE_TOKEN'];
+    }
+    else {
+      $token = Settings::get('upaknee_token');
+    }
+
+    return $token;
+  }
+
+  /**
+   * Send email via Postmark API.
+   *
+   * @param array $subscriber_data
+   *   The array containing Upaknee API needed fields.
+   */
+  public function subscriberAdd(array $subscriber_data) {
+    $username = $this->getToken();
+    $password = '';
+    $subscriber_data_xml = '<subscriber>
+                              <email>' . $subscriber_data["email"] . '</email>
+                              <existing-update>true</existing-update>
+                              <zipcode>' . $subscriber_data["zipcode"] . '</zipcode>
+                              <subscriptions>
+                                  <subscription>
+                                      <newsletter-id>' . $subscriber_data["list"] . '</newsletter-id>
+                                  </subscription>
+                              </subscriptions>
+                            </subscriber>';
+    $ch = curl_init();
+    curl_setopt_array($ch, [
+      CURLOPT_URL => "https://rest.upaknee.com/subscribers/",
+      CURLOPT_RETURNTRANSFER => TRUE,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_USERPWD => $username . ":" . $password,
+      CURLOPT_FOLLOWLOCATION => TRUE,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "POST",
+      CURLOPT_POSTFIELDS => $subscriber_data_xml,
+      CURLOPT_HTTPHEADER => [
+        "Accept: application/xml",
+        "Content-Type: application/xml"
+      ],
+    ]);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    $response_array = [
+      'status' => 'success',
+      'subscriber' => 'true',
+      'response' => $response,
+    ];
+
+    return $response_array;
+
+  }
+
+  /**
+   * Begin script and API operations.
+   *
+   * @param string $operation
+   *   The operation being called via the endpoint uri.
+   */
+  public function begin(string $operation) {
+    // Get POST data and check operation.
+    $this->operation = $operation;
+
+    $request_method = $this->request->getCurrentRequest()->getMethod();
+    if ($request_method == "POST") :
+      $data = $this->request->getCurrentRequest()->get('subscriber');
+
+      if ($operation == 'subscribe') :
+        $response_array = $this->subscriberAdd($data);
+      endif;
+
+    else :
+
+      $response_array = [
+        'status' => 'error',
+        'response' => 'no post data',
+      ];
+
+    endif;
+
+    $response = new CacheableJsonResponse($response_array);
+    return $response;
+  }
+
+}
+
+// End PostmarkAPI class.
