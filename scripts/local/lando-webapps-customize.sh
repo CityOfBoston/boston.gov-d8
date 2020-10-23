52dddc43c3cff3f49df2ed1ded1e215fadfdc0e5
David Upton <davidrkupton@gamil.com>
2020-10-23 11:18:56 -0400

    Fixes symlinks for webapps.

--- scripts/local/lando-webapps-customize.sh
+++ scripts/local/lando-webapps-customize.sh
@@ -12,22 +12,26 @@
     target_env="local"
 
     printf "\n"
-    printout "SCRIPT" "starts <$(basename $BASH_SOURCE) >\n"
+    printout "SCRIPT" "starts <$(basename $BASH_SOURCE)>\n"
 
     printout "INFO" "Check and create development directory links for WebApps."
 
     # Link the webapps folder from inside drupal into the root, so that
     # developers can find the modules more easily.
-    if [[ ! -h ${webapps_local_local_dir} ]]; then
-      ln -s ${webapps_local_source} ${webapps_local_local_dir}
+    if [[ ! -h ${REPO_ROOT}/${webapps_local_local_dir} ]]; then
+      cd ${REPO_ROOT} &&
+        ln -s ${webapps_local_source} ${webapps_local_local_dir}
+    fi
     printf "      Webapps directory is mapped to ${webapps_local_local_dir}.\n"
 
     # Create a link for global.package file so it can be used by npm to set up the container
     # with a default set of node tools.
-    if [[ ! -h ${webapps_local_local_dir}/package.json ]]; then
-      ln -s ${REPO_ROOT}/scripts/global.package ${webapps_local_local_dir}/package.json
+    if [[ ! -h ${REPO_ROOT}/${webapps_local_local_dir}/package.json ]]; then
+      cd ${REPO_ROOT} &&
+        ln -s scripts/global.package ${webapps_local_local_dir}/package.json
+    fi
     printf "      Global node package file mapped.\n"
 
     printout "SUCCESS" "Directory links created and/or verified.\n"
 
-    printout "SCRIPT" "ends <$(basename $BASH_SOURCE) >\n\n"
+    printout "SCRIPT" "ends <$(basename $BASH_SOURCE)>\n"
