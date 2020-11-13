c28001bc3a194d73565766537409a6f507e11fb9
David Upton <davidrkupton@gamil.com>
2020-11-13 10:28:58 -0500

    Release 1 for bostonaddtocalview extension.

--- docroot/modules/custom/bos_core/src/Plugin/Field/FieldFormatter/BosAddtocalView.php
+++ docroot/modules/custom/bos_core/src/Plugin/Field/FieldFormatter/BosAddtocalView.php
@@ -5,6 +5,7 @@ namespace Drupal\bos_core\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\addtocal\Plugin\Field\FieldFormatter\AddtocalView;
+use Spatie\CalendarLinks\Link;
 
 /**
  * Boston Add to Cal view formatter.
@@ -24,7 +25,40 @@ use Drupal\addtocal\Plugin\Field\FieldFormatter\AddtocalView;
 class BosAddtocalView extends AddtocalView {
 
   public function viewElements(FieldItemListInterface $items, $langcode) {
-    $elements = parent::viewElements($items,$langcode);
+    $elements = parent::viewElements($items, $langcode);
+
+    # The following is a fix fot the date_recur field type.
+    if (isset($elements) && (count($elements) > 0) && getClass($items) == "Drupal\date_recur\Plugin\Field\FieldType\DateRecurFieldItemList") {
+      foreach ($items as $delta => $item) {
+        $helper = $item->getHelper();
+        // $rules = $helper->getRules();
+        $now = new \DateTime();
+        $events = $helper->getOccurrences($now, NULL);
+
+        if ($events) {
+          $next_event = $events[0];
+          $start_date = $next_event->getStart();
+          $end_date = $next_event->getEnd() ?? $start_date;
+
+          $elements[$delta]['start_date']['#plain_text'] = $this->formatDate($start_date);
+          $elements[$delta]['end_date']['#plain_text'] = $this->formatDate($end_date);
+
+          $title = $elements[$delta]["addtocal"]["#addtocal_link"]->title;
+          $address = $elements[$delta]["addtocal"]["#addtocal_link"]->address;
+          $description = $elements[$delta]["addtocal"]["#addtocal_link"]->description;
+          $link = Link::create($title, $start_date, $end_date);
+          $link->address($address);
+          $link->description($description);
+          $elements[0]["addtocal"]["#addtocal_link"] = $link;
+
+          $elements[$delta]["addtocal"]["#access"] = TRUE;
+        }
+        else {
+          $elements[$delta]["addtocal"]["#access"] = FALSE;
+        }
+      }
+    }
+
     return $elements;
   }
 }
