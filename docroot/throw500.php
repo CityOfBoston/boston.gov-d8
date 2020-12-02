bcae066c5a9a49e23a126feca4a84dd9b2d2e23d
David Upton <davidrkupton@gamil.com>
2020-12-02 11:54:33 -0500

    Creates 500 error thrower and default page.

--- docroot/throw500.php
+++ docroot/throw500.php
@@ -1,2 +1,5 @@
 <?php
-  throw new Exception('COB Test 500 Error!');
+//  throw new Exception('COB Test 500 Error!');
+header('HTTP/1.1 500 Service Temporarily Unavailable', true, 500);
+header('Status: 500 Service Temporarily Unavailable', true, 500);
+header('Retry-After: 300');//300 seconds
