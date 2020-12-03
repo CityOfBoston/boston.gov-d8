3605162488834757554636de761b714f9e68e11a
Matthew McGowan <matthew.mcgowan@boston.gov>
2020-12-03 09:53:34 -0500

    Abutters again to try and fix deploy to Dev2 env in Acquia.

---
+++ docroot/modules/custom/bos_components/modules/bos_web_app/apps/abutters/src/js/app/config.js
@@ -0,0 +1,7 @@
+export function configVals() {
+	return {
+		"publicRuntimeConfig" : {
+		   "MapboxAccessToken": "pk.eyJ1IjoiY2l0eW9mYm9zdG9uIiwiYSI6ImNqMTd1dDdqZTA1c2UyeHFzNGhrN2g0bHYifQ.SZ8J0aXwDHt4sCmZ9rQO2A",
+		}
+	}
+}
