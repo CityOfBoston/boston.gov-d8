c2152c6e5548b765f115724c95ded61020ff9589
Alex Dora <pda5@3ds.com>
2020-12-18 00:05:04 -0500

    Updates to BH Maps - Project Type Views fields

---
+++ docroot/modules/custom/bos_content/modules/node_buildinghousing/src/Plugin/views/field/BuildingHousingProjectTypeMarkerViewsField.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\node_buildinghousing\Plugin\views\field;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Utility\Random;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+
+/**
+ * A handler to provide a field that is completely custom by the administrator.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("building_housing_project_type_marker_views_field")
+ */
+class BuildingHousingProjectTypeMarkerViewsField extends BuildingHousingProjectTypeViewsField
+{
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render(ResultRow $values)
+  {
+
+    $mainType = $this->getMainProjectTypeName($values->_entity);
+
+    if ($mainType) {
+
+      switch ($mainType) {
+        case "Housing":
+          $projectName = 'housing-marker.svg';
+          break;
+        case "Open Space":
+          $projectName = 'open-space-marker.svg';
+          break;
+        case "Business":
+          $projectName = 'business-marker.svg';
+          break;
+        default:
+          $projectName = 'other-marker.svg';
+          break;
+
+      }
+
+      return $projectName;
+    }
+  }
+
+}
--- docroot/modules/custom/bos_content/modules/node_buildinghousing/src/Plugin/views/field/BuildingHousingProjectTypeViewsField.php
+++ docroot/modules/custom/bos_content/modules/node_buildinghousing/src/Plugin/views/field/BuildingHousingProjectTypeViewsField.php
@@ -14,26 +14,30 @@ use Drupal\views\ResultRow;
  *
  * @ViewsField("building_housing_project_type_views_field")
  */
-class BuildingHousingProjectTypeViewsField extends FieldPluginBase {
+class BuildingHousingProjectTypeViewsField extends FieldPluginBase
+{
 
   /**
    * {@inheritdoc}
    */
-  public function usesGroupBy() {
+  public function usesGroupBy()
+  {
     return FALSE;
   }
 
   /**
    * {@inheritdoc}
    */
-  public function query() {
+  public function query()
+  {
     // Do nothing -- to override the parent query.
   }
 
   /**
    * {@inheritdoc}
    */
-  protected function defineOptions() {
+  protected function defineOptions()
+  {
     $options = parent::defineOptions();
 
     $options['hide_alter_empty'] = ['default' => FALSE];
@@ -43,58 +47,61 @@ class BuildingHousingProjectTypeViewsField extends FieldPluginBase {
   /**
    * {@inheritdoc}
    */
-  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+  public function buildOptionsForm(&$form, FormStateInterface $form_state)
+  {
     parent::buildOptionsForm($form, $form_state);
   }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function render(ResultRow $values) {
-    // Return a random text, here you can include your custom logic.
-    // Include any namespace required to call the method required to generate
-    // the desired output.
-//    $random = new Random();
-//    return $random->name();
-
+  public function getMainProjectTypeName($projectEntity)
+  {
     $mainType = null;
 
     $termStorage = \Drupal::entityTypeManager()->getStorage('taxonomy_term');
-    $entity = $values->_entity;
 
-    if ($dispositionTypeId = $entity->get('field_bh_disposition_type')->target_id) {
+
+    if ($dispositionTypeId = $projectEntity->get('field_bh_disposition_type')->target_id) {
       $dispositionTypeParents = $termStorage->loadAllParents($dispositionTypeId);
       $mainType = !empty($dispositionTypeParents) ? array_pop($dispositionTypeParents) : null;
     }
 
-    if ($projectTypeId = $entity->get('field_bh_project_type')->target_id) {
-      if ( empty($mainType) || $mainType->getName() == 'Housing') {
+    if ($projectTypeId = $projectEntity->get('field_bh_project_type')->target_id) {
+      if (empty($mainType) || $mainType->getName() == 'Housing') {
         $mainType = 'Housing';
       }
     }
 
     if ($mainType) {
-      $mainType = is_string($mainType) ? $mainType : $mainType->getName();
-
-//    $projectType = 'Housing';
-
-    switch ($mainType) {
-      case "Housing":
-        $iconType = 'maplist-housing';
-        break;
-      case "Open Space":
-        $iconType = 'maplist-open-space';
-        break;
-      case "Business":
-        $iconType = 'maplist-business';
-        break;
-      default:
-        $iconType = 'maplist-other';
-        break;
-
+      return is_string($mainType) ? $mainType : $mainType->getName();
     }
 
-    return \Drupal::theme()->render("bh_icons", ['type' => $iconType]);
+    return $mainType;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render(ResultRow $values)
+  {
+    $mainType = $this->getMainProjectTypeName($values->_entity);
+
+    if ($mainType) {
+
+      switch ($mainType) {
+        case "Housing":
+          $iconType = 'maplist-housing';
+          break;
+        case "Open Space":
+          $iconType = 'maplist-open-space';
+          break;
+        case "Business":
+          $iconType = 'maplist-business';
+          break;
+        default:
+          $iconType = 'maplist-other';
+          break;
+      }
+
+      return \Drupal::theme()->render("bh_icons", ['type' => $iconType]);
 
     }
   }
