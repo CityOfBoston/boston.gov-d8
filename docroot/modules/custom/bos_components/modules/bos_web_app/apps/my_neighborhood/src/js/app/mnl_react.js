27d9c4555f6566773a9b8692c7fc82f847da45c0
stella.ubaha@boston.gov <stella.ubaha@boston.gov>
2020-12-29 11:13:48 -0500

    Changing back travis and config files be/4 push to develop branch

--- docroot/modules/custom/bos_components/modules/bos_web_app/apps/my_neighborhood/src/js/app/mnl_react.js
+++ docroot/modules/custom/bos_components/modules/bos_web_app/apps/my_neighborhood/src/js/app/mnl_react.js
@@ -183,7 +183,11 @@ class MNL extends React.Component {
           "headers": {
             "Content-Type": "application/json",
             // Needed for CORS google translate.
-            "Access-Control-Allow-Origin": "https://translate.googleusercontent.com"
+            "Access-Control-Allow-Origin": "https://translate.googleusercontent.com",
+            "Access-Control-Allow-Credentials": true,
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "x-requested-with",
+            "Access-Control-Max-Age": "1728000"
           },
         },
 
@@ -241,7 +245,11 @@ class MNL extends React.Component {
           "headers": {
             "Content-Type": "application/json",
             // Needed for CORS google translate
-            "Access-Control-Allow-Origin": "https://translate.googleusercontent.com"
+            "Access-Control-Allow-Origin": "https://translate.googleusercontent.com",
+            "Access-Control-Allow-Credentials": true,
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "x-requested-with",
+            "Access-Control-Max-Age": "1728000"
           },
         },
     )
