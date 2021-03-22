da24eef76c646367e8244569a96c2ee49f07f0c4
Matthew McGowan <matthew.mcgowan@boston.gov>
2021-03-22 12:01:20 -0400

    Latest.

---
+++ docroot/modules/custom/bos_components/modules/bos_web_app/apps/assessing/src/js/components/Search.js
@@ -0,0 +1,38 @@
+
+
+class Search extends React.Component {
+  render() {
+      return (
+        <div className="sf">
+          <div className="sf-i">
+          <form id="mnl-search-form" onSubmit={this.props.handleKeywordSubmit}>
+            <input
+                type="text"
+                id="mnl-search-input"
+                value={this.props.currentKeywords}
+                style={this.props.styleInline}
+                aria-label={this.props.placeholder}
+                placeholder={this.props.placeholder}
+                className={this.props.searchClass + ' search'}
+                onChange={this.props.handleKeywordChange}
+            />
+
+            <button
+              type="button"
+              id="mnl-search-button"
+              className="sf-i-b"
+              onClick={this.props.handleKeywordSubmit}
+            >
+              Search
+            </button>
+          </form>
+          </div>
+          <div className="sf-i resize">
+            <span style ={this.props.styleInline} className={this.props.searchClass}>{this.props.currentKeywords}</span>
+          </div>
+        </div>
+      )
+  }
+}
+
+
