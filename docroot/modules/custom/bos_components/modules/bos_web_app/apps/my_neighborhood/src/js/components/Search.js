711bd69346b9ee4b0e48eff2604700e81c89ef69
Phillip Kelly <phillipbentonkelly@gmail.com>
2021-05-18 14:34:54 -0400

    Web Apps: Assessing - removing changes to my neighborhood app

--- docroot/modules/custom/bos_components/modules/bos_web_app/apps/my_neighborhood/src/js/components/Search.js
+++ docroot/modules/custom/bos_components/modules/bos_web_app/apps/my_neighborhood/src/js/components/Search.js
@@ -1,10 +1,12 @@
+
+
 class Search extends React.Component {
   render() {
       return (
         <div className="sf">
           <div className="sf-i">
-            <form id="mnl-search-form" onSubmit={this.props.handleKeywordSubmit}>
-              <input
+          <form id="mnl-search-form" onSubmit={this.props.handleKeywordSubmit}>
+            <input
                 type="text"
                 id="mnl-search-input"
                 value={this.props.currentKeywords}
@@ -13,25 +15,20 @@ class Search extends React.Component {
                 placeholder={this.props.placeholder}
                 className={this.props.searchClass + ' search'}
                 onChange={this.props.handleKeywordChange}
-              />
+            />
 
-              <button
-                type="button"
-                id="mnl-search-button"
-                className="sf-i-b"
-                onClick={this.props.handleKeywordSubmit}
-              >
-                Search
-              </button>
-            </form>
+            <button
+              type="button"
+              id="mnl-search-button"
+              className="sf-i-b"
+              onClick={this.props.handleKeywordSubmit}
+            >
+              Search
+            </button>
+          </form>
           </div>
           <div className="sf-i resize">
-            <span
-              style ={this.props.styleInline}
-              className={this.props.searchClass}
-            >
-              {this.props.currentKeywords}
-            </span>
+            <span style ={this.props.styleInline} className={this.props.searchClass}>{this.props.currentKeywords}</span>
           </div>
         </div>
       )
