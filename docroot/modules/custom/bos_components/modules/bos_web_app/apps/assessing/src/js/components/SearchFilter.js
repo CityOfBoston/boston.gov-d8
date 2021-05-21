9048346f545eabc396b104b7d411c067c1aa8486
Phillip Kelly <phillipbentonkelly@gmail.com>
2021-05-21 10:46:06 -0400

    Web Apps: Assessing Search/Results UI/UX - Componentized searchFilter RationButtons

---
+++ docroot/modules/custom/bos_components/modules/bos_web_app/apps/assessing/src/js/components/SearchFilter.js
@@ -0,0 +1,36 @@
+class SearchFilters extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    const RadioBtnElems = () => {
+      const fn_onChange = this.props.onChange;
+      // console.log('onChange: ', this.props.onChange());
+
+      return this.props.searchFilters.map((obj, index) => {
+        let attributes = {
+          id: `radio[${index}]`,
+          type: 'radio',
+          name: index,
+          className: 'ra-f',
+          labelText: obj.label,
+          onChange: {fn_onChange},
+        };
+
+        if (this.props.searchByFilter === index)
+          attributes['checked'] = 'checked';
+
+        return <RadioBtn {...attributes} />;
+      });
+    }
+
+    return(
+      <div className="search-filters">
+        <label className="filters-label">Search By:</label>
+
+        {RadioBtnElems()}
+      </div>
+    );
+  }
+}
