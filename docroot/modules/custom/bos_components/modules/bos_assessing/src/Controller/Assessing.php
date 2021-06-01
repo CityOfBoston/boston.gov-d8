30850aa877e34c2802275ae353670b3f94656e1d
Matthew McGowan <44584567+mmcgowanBOS@users.noreply.github.com>
2021-06-01 12:22:26 -0400

    Update Assessing.php

--- docroot/modules/custom/bos_components/modules/bos_assessing/src/Controller/Assessing.php
+++ docroot/modules/custom/bos_components/modules/bos_assessing/src/Controller/Assessing.php
@@ -53,7 +53,7 @@ class Assessing extends ControllerBase {
    *   The id of the parcel requested by user
    */
   public function lookupParcel($parcel_id) {
-      $url = 'https://data.boston.gov/api/3/action/datastore_search_sql?sql=SELECT%20*%20from%20%228de4e3a0-c1d2-47cb-8202-98b9cbe3bd04%22%20WHERE%20%22PID%22%20LIKE%20%27'.$parcel_id.'%27';
+      $url = 'https://data.boston.gov/api/3/action/datastore_search_sql?sql=SELECT%20*%20from%20%22c4b7331e-e213-45a5-adda-052e4dd31d41%22%20WHERE%20%22PID%22%20LIKE%20%27'.$parcel_id.'%27';
       // Make the request and return the response.
       $ch = curl_init();
       curl_setopt($ch, CURLOPT_URL, $url);
