b81eb6b4cfa46e2cd481d94eace69ee0c26aa368
Matthew McGowan <matthew.mcgowan@boston.gov>
2021-06-03 12:07:26 -0400

    JS timer and wait.

--- docroot/modules/custom/bos_components/modules/bos_assessing/js/assessing_map.js
+++ docroot/modules/custom/bos_components/modules/bos_assessing/js/assessing_map.js
@@ -1,37 +1,35 @@
-  (function ($, Drupal, window, document) {
-    $(document).ready(function () {
-        if (typeof Drupal.geolocation !== 'undefined') {
-                    waintUntilMapIsAvailable();
-        }
-        function waintUntilMapIsAvailable(){
-            if (Drupal.geolocation.maps === undefined) {
-                setTimeout(waintUntilMapIsAvailable, 200);
-            } else {
-                const coords = drupalSettings.bos_assessing.coords[0];
-                const polyCoords = [];
+function runMap() {
+    const coords = drupalSettings.bos_assessing.coords[0];
+    const polyCoords = [];
 
-                $(coords).each(function(index,value) {
-                    polyCoords.push( {"lng" : value[0], "lat" : value[1]} );
-                });
+    jQuery(coords).each(function(index,value) {
+        polyCoords.push( {"lng" : value[0], "lat" : value[1]} );
+    });
 
-                const map = new google.maps.Map(document.getElementById("map-container"), {
-                    zoom: 18,
-                    center: polyCoords[0],
-                    mapTypeId: 'satellite',
-                    tilt: 0,
-                });
+    const map = new google.maps.Map(document.getElementById("map-container"), {
+        zoom: 18,
+        center: polyCoords[0],
+        mapTypeId: 'satellite',
+        tilt: 0,
+    });
 
-                // Construct the polygon.
-                const parcel = new google.maps.Polygon({
-                    paths: polyCoords,
-                    strokeColor: "#FF0000",
-                    strokeOpacity: 0.8,
-                    strokeWeight: 2,
-                    fillColor: "#FF0000",
-                    fillOpacity: 0.35,
-                });
-                parcel.setMap(map);
-            }
-        }
-    })    
-})(jQuery, Drupal, this, this.document);
+    // Construct the polygon.
+    const parcel = new google.maps.Polygon({
+        paths: polyCoords,
+        strokeColor: "#FF0000",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "#FF0000",
+        fillOpacity: 0.35,
+    });
+    parcel.setMap(map);
+}
+
+function waintUntilMapIsAvailable(){
+    if (Drupal.geolocation.maps !== undefined) {
+        clearInterval(checkGeo);
+        runMap();
+    } 
+}
+
+const checkGeo = setInterval(waintUntilMapIsAvailable, 500);
