73ecfffba8d63793cd9cc3a88e05b48b01880595
Phillip Kelly <phillipbentonkelly@gmail.com>
2021-07-16 18:08:43 -0400

    Web App (Assessing): Re-doing styles to ... wip 3

--- docroot/modules/custom/bos_components/modules/bos_web_app/apps/assessing/src/js/components/Search.js
+++ docroot/modules/custom/bos_components/modules/bos_web_app/apps/assessing/src/js/components/Search.js
@@ -1,19 +1,30 @@
 class Search extends React.Component {
   render () {
+    const SearchInputElem = () => {
+      let attributes = {
+        type: this.props.type,
+        id: this.props.id,
+        value: this.props.value,
+        placeholder: this.props.placeholder,
+        ['aria-label']: this.props.placeholder,
+        onChange: this.props.handleKeywordChange,
+        className: ' search',
+      };
+  
+      if (this.props.searchClass)
+        attributes['className'] = `${this.props.searchClass} search`
+  
+      if (this.props.style)
+        attributes['style'] = this.props.styleInline;
+
+      return <input { ...attributes} />
+    }
+
     return (
       <div className="sf">
         <div className="sf-i">
           <form id="mnl-search-form" onSubmit={this.props.handleKeywordSubmit}>
-            <input
-              type="text"
-              id="mnl-search-input"
-              value={this.props.currentKeywords}
-              style={this.props.styleInline}
-              aria-label={this.props.placeholder}
-              placeholder={this.props.placeholder}
-              className={this.props.searchClass + ' search'}
-              onChange={this.props.handleKeywordChange}
-            />
+            {SearchInputElem()}
 
             <button
               type="button"
@@ -26,9 +37,15 @@ class Search extends React.Component {
           </form>
         </div>
         <div className="sf-i resize">
-          <span style={this.props.styleInline} className={this.props.searchClass}>{this.props.currentKeywords}</span>
+          <span style={this.props.styleInline} className={this.props.searchClass}>{this.props.value}</span>
         </div>
       </div>
     )
   }
+}
+
+Search.defaultProps = {
+  type: 'text',
+  id: 'mnl-search-input',
+  className: ' search',
 }
