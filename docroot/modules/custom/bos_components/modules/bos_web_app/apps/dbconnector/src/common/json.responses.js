9d683a83ebad8f3ec85df207c64e4708c0586b83
David Upton <davidrkupton@gmail.com>
2021-07-20 11:11:31 -0400

    std response.

--- docroot/modules/custom/bos_components/modules/bos_web_app/apps/dbconnector/src/common/json.responses.js
+++ docroot/modules/custom/bos_components/modules/bos_web_app/apps/dbconnector/src/common/json.responses.js
@@ -1,7 +1,12 @@
-exports.json_response = (res, http_code, response, type) => {
+exports.json_response = (res, http_code, response) => {
   res.status(http_code).send(response);
 };
 
 exports.json_error = (res, http_code, message) => {
-  res.status(http_code).send(message)
+  if (!message || typeof message === "undefined" || message == "") {
+    res.status(http_code).send();
+  }
+  else {
+    res.status(http_code).send(message);
+  }
 };
