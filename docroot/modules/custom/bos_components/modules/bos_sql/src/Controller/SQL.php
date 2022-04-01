5c8d1167ee183d88b1e1ce7a259aab0fbc99bd30
David Upton <davidrkupton@gmail.com>
2022-04-01 09:07:24 -0400

    Updates to SQL component.

--- docroot/modules/custom/bos_components/modules/bos_sql/src/Controller/SQL.php
+++ docroot/modules/custom/bos_components/modules/bos_sql/src/Controller/SQL.php
@@ -5,46 +5,64 @@ namespace Drupal\bos_sql\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Site\Settings;
 use Drupal\Core\Cache\CacheableJsonResponse;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class SQL.
  *
  * @package Drupal\bos_assessing\Controller
+ *
+ * @see https://docs.boston.gov/digital/guides/amazon-web-services/services/microservices-2021/sql-proxy-2021
  */
 class SQL extends ControllerBase {
 
-  /**
-   * Class var.
-   *
-   * @var \Symfony\Component\HttpFoundation\RequestStack
-   */
-  public $request;
+  public const AUTH_TOKEN = 0;
+  public const CONN_TOKEN = 1;
 
+  /**
+   * @var string pointer to the DBConnector endpoint-url used by this instance
+   */
+  private $base_url = "";
+
+  /**
+   * @var bool Flag as to the return format. If json=true then returns a jsonrequest
+   */
+  private $json = FALSE;
+
+  /**
+   * @inheritDoc
+   */
+  public function __construct(bool $json = FALSE) {
+    $this->base_url = 'https://dbconnector.' . $this->checkLocalEnv() . 'boston.gov/v1';
+    $this->json = $json;
+  }
 
   /**
    * Check for local env and set connector url staging option.
    */
   public function checkLocalEnv() {
     $local = (isset($_ENV['DBCONNECTOR_SETTINGS'])) ? '' : 'digital-staging.';
-
     return $local;
   }
 
   /**
    * Run query against SQL database and return JSON response.
+   * @see https://docs.boston.gov/digital/guides/amazon-web-services/services/microservices-2021/sql-proxy-2021#execute-sql-statement
+   *
+   * @param $bearer_token string An active auth token
+   * @param $connection_token string A valid connection string token
+   * @param string $statement string A valid SQL statement
+   *
+   * @return array|string
    */
-  public function runQuery($bearer_token,$connection_token,$statement) {
-    $post_fields = [
+  public function runQuery(string $bearer_token, string $connection_token, string $statement) {
+    $post_fields = json_encode([
       "token"  => $connection_token,
       "statement" => $statement,
-    ];
-    $post_fields = json_encode($post_fields);
-    $url = 'https://dbconnector.' . $this->checkLocalEnv() . 'boston.gov/v1/query/mssql';
+    ]);
 
     // Make the request and return the response.
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_URL, $this->base_url . '/query/mssql');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
@@ -54,12 +72,12 @@ class SQL extends ControllerBase {
       "Authorization: Bearer " . $bearer_token,
     ]);
     $info = curl_exec($ch);
-    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
+//    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
     if (isset($info)) {
       $data = json_decode($info);
-    } else {
+    }
+    else {
       $data = "error connecting to service";
     }
 
@@ -67,7 +85,24 @@ class SQL extends ControllerBase {
     return $data;
   }
 
-  public function runSelect($bearer_token,$connection_token,$table,$filter,$sort,$limit,$page,$fields) {
+  /**
+   * Execute an abstracted select statement.
+   * Works on SQL Database conn strings - simple query without concept of joins.
+   *
+   * @see https://docs.boston.gov/digital/guides/amazon-web-services/services/microservices-2021/sql-proxy-2021#run-select-query
+   *
+   * @param $bearer_token string An active auth token
+   * @param $connection_token string A valid connection string token
+   * @param $table string The table to extract data from
+   * @param $fields array
+   * @param $filter string
+   * @param $sort array
+   * @param $limit string
+   * @param $page string
+   *
+   * @return \Drupal\Core\Cache\CacheableJsonResponse json encoded recordset
+   */
+  public function runSelect(string $bearer_token, string $connection_token, string $table, array $fields, array $filter, array $sort, string $limit, string $page) {
 
     $post_fields = [
       "token"  => $connection_token,
@@ -90,11 +125,10 @@ class SQL extends ControllerBase {
     }
 
     $post_fields = json_encode($post_fields);
-    $url = 'https://dbconnector.' . $this->checkLocalEnv() . 'boston.gov/v1/select/mssql';
 
     // Make the request and return the response.
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_URL, $this->base_url . '/select/mssql');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
@@ -104,12 +138,13 @@ class SQL extends ControllerBase {
       "Authorization: Bearer " . $bearer_token,
     ]);
     $info = curl_exec($ch);
-    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+//    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
 
     if (isset($info)) {
       $data = json_decode($info);
-    } else {
+    }
+    else {
       $data = "error connecting to service";
     }
 
@@ -119,9 +154,57 @@ class SQL extends ControllerBase {
   }
 
   /**
-   * Request DB operations
-   * * @param string $app_name
-   *   The name of the web application / service.
+   * Execute a stored procedure.
+   *
+   * @see https://docs.boston.gov/digital/guides/amazon-web-services/services/microservices-2021/sql-proxy-2021#execute-stored-procedure
+   *
+   * @param $bearer_token string An active auth token
+   * @param $connection_token string A valid connection string token
+   * @param $proc_name string The procedure to execute
+   * @param $params array Parameters to pass to the procedure - key=>value pairs
+   * @param $output array Ouput params for the procedure - key=>value pairs
+   *
+   * @return \Drupal\Core\Cache\CacheableJsonResponse json encoded results of the stored proc execution, or a message
+   */
+  public function runProcedure(string $bearer_token, string $connection_token, string $proc_name, array $params, array $output) {
+    $post_fields = json_encode([
+      "token"  => $connection_token,
+      "procname" => $proc_name,
+      "params" => $params,
+      "output" => $output,
+    ]);
+
+    // Make the request and return the response.
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $this->base_url . '/exec/mssql');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+      "Accept: application/json",
+      "Content-Type: application/json",
+      "Authorization: Bearer " . $bearer_token,
+    ]);
+    $info = curl_exec($ch);
+//    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    if (isset($info)) {
+      $data = json_decode($info);
+    }
+    else {
+      $data = "error connecting to service";
+    }
+
+    $response = new CacheableJsonResponse($data);
+    return $response;
+  }
+
+  /**
+   * Connect to DBConnector and authenticate
+   *
+   * @param string $app_name The name of the web application / service.
+   *
+   * @return array|null An array containing the auth and conn tokens.
    */
   public function getToken($app_name) {
 
@@ -141,16 +224,14 @@ class SQL extends ControllerBase {
       ];
     }
 
-    $post_fields = [
+    $post_fields = json_encode([
       "username" => $dbconnector_env["username_" . $app_name],
       "password" => $dbconnector_env["password_" . $app_name],
-    ];
-    $post_fields = json_encode($post_fields);
-    $url = 'https://dbconnector.' . $this->checkLocalEnv() . 'boston.gov/v1/auth';
+    ]);
 
     // Make the request and return the response.
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_URL, $this->base_url . '/auth');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
@@ -159,12 +240,16 @@ class SQL extends ControllerBase {
       "Content-Type: application/json",
     ]);
     $info = curl_exec($ch);
-    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+//    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
     if (isset($info)) {
       $data = json_decode($info,true);
-      $data = [$data["authToken"],$dbconnector_env["conntoken_" . $app_name],];
-    } else {
+      $data = [
+        self::AUTH_TOKEN => $data["authToken"],
+        self::CONN_TOKEN => $dbconnector_env["conntoken_" . $app_name],
+      ];
+    }
+    else {
       $data = null;
     }
 
