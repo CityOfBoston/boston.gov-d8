c9c16d005f1b61a12016f31779c3972f43382c8f
David Upton <davidrkupton@gmail.com>
2022-08-11 12:49:19 -0400

    Update notes on config form.

--- docroot/modules/custom/bos_components/modules/bos_mnl/src/Form/MNLSettingsForm.php
+++ docroot/modules/custom/bos_components/modules/bos_mnl/src/Form/MNLSettingsForm.php
@@ -207,7 +207,68 @@ class MNLSettingsForm extends ConfigFormBase {
           '#title' => 'App Overview',
           '#collapsible' => TRUE,
           'doc' => [
-            "#markup" => ""
+            "#markup" => Markup::create("
+              <h4>App Function</h4>
+              This app is used to syncronise geo-tagged data between City of
+              Boston managed authoritative sources and Drupal.<br>
+              The data received is used in the My Neighborhood Lookup app on the
+              website, but is stored as a 'neighborhood_lookup' node, so it can
+              be used for any purpose on the website.<br>
+              The data itself is community information associated with physical
+              addresses in the City of Boston. It is extracted and compiled from
+              a wide variety of CoB internal databases and data sources.
+              <h4>Synchronisation Process</h4>
+              The actual syncronisation is performed between Civis and Drupal,
+              with Civis initiating and push the data on a schedule (see below).
+              <br>
+              There are approx 400,000 unique address records being managed with
+              the unique address identifier for each record being a CoB
+              generated <b>'SAM ID'</b>.<br>
+              The SAM ID is a GIS identifier and which has a 1:1 relationship
+              with a physical property address.<br><br>
+              There are 2 syncronization processes:
+              <ol>
+                <li>
+                  <b>Differential Updates:</b> Updates a set of SAM records
+                  which have changed in some way from the previous differential
+                  update.<br>
+                  <span style='color:blue'><i>Differential updates are sent
+                  daily at 9 PM (EST)</i></span>
+                </li>
+                <li>
+                  <b>Full Imports:</b> Updates the complete SAM recordset
+                  which is sent to be sure that Drupal is properly sync'd and to
+                  allow Drupal to identify records that have been deleted.<br>
+                  <span style='color:blue'><i>Full Imports are sent on the last
+                  day of each month at 9 PM (EST)<br>
+                  (Differential update is not sent on this day)</i></span>
+                </li>
+              </ol>
+              <h4>Workflow</h4>
+              This workflow applies for both differential updates and full
+              imports.
+              <ol>
+                <li>
+                  When Civis pushes to Drupal, the data is loaded into a queue.
+                  <br>
+                  Civis transfers the data in chunks of 50,000 records, and when
+                  the transfer is complete, sends a completion message.
+                </li>
+                <li>
+                  Every 5 mins Drupal checks the queue. If the transfer is
+                  complete, then Drupal starts to process the queue in chunks.
+                  <br>
+                  Processing means that the data in the queue is compared to the
+                  data held in the node for that SAM ID, and is created or
+                  updated as needed.
+                </li>
+                <li>
+                  (Full Import only) (optional - see configuration)<br>
+                  At the end of the processing, Drupal deletes the nodes which
+                  were not updated.
+                </li>
+              </ol>
+            ")
           ],
         ],
         'configuration' => [
@@ -215,7 +276,33 @@ class MNLSettingsForm extends ConfigFormBase {
           '#title' => 'Configuration Settings',
           '#collapsible' => TRUE,
           'doc' => [
-            "#markup" => ""
+            "#markup" => Markup::create("
+              <h4>API Key/Token</h4>
+              This is a key which Civis must use to authenticate when sending
+              data to Drupal.<br>
+              <span style='color:blue'><i>Changing this key is immediate and the
+              previous key is deleted.</i></span><br>
+              <b>All API communications must use the new key immediately after
+              it is changed.</b>
+              <h4>Use Entity Manager</h4>
+              For performance, the syncronization interracts directly with the
+              database and does not use the Drupal framework to update the data
+              in the 'neighborhood_lookup' nodes.<br>
+              Selecting this box forces the synchronization to use the Drupal
+              entity manager, which is slower, but is likely to be more robust
+              as Drupal releases future versions.
+              <h4>Purge SAM ID's</h4>
+              Selecting this checkbox will cause the synchronization process to
+              purge (cleanup) records at the end of a full import.<br>
+              If a record has not been updated during a full impoort, it clearly
+              has been deleted from the CoB records and should be deleted from
+              Drupal.<br>
+              Enabling this function deletes any records that were not updated
+              during the full import, or by any of the differential imports in
+              the preceeding 7 days.<br>
+              Disabling the function means no purging occurs after a full
+              import.
+            ")
           ],
         ],
         'api_notes' => [
@@ -224,42 +311,78 @@ class MNLSettingsForm extends ConfigFormBase {
           '#collapsible' => TRUE,
           'doc' => [
             "#markup" => "
-              All POSTS to endpoint must have format:
-                <ul><pre>/rest/mnl/YYY?api_key=XXX</pre></ul>
-                <ul><li>Where YYYY is one of 'update', 'import' or 'manual'.</li>
-                <li>Where api_key XXX is the <i><b>API KEY / token</b></i> defined above.</li></ul><br>
-              'import' and 'update' endpoint usage must include a json string in the
-                body in the format:
-              <ul><pre>[
-                  {\"sam_address_id\":123,\"full_address\":\"address\",\"data\":jsonstring\"},
-                  {\"sam_address_id\":123,\"full_address\":\"address\",\"data\":jsonstring\"}
-              ]</pre></ul>
-                <ul><li>The 'jsonstring' must be a valid json object in string form and can contain
-                any data provided it represents a single record and can be decoded by
-                PHP's json_decode function.</li></ul><br>
-              'manual' endpoint requires no body (any body will be ignored) but must have an extended querystring in the format:
-                <ul><pre>?api_key=XXX&path=/path-on-server/file.json&limit=N&mode=ZZZ</pre></ul>
+              All POSTS to endpoint must have format:<br><br>
+              <pre>/rest/mnl/YYY?api_key=XXX</pre><br>
+              <ul>
+                <li>Where YYYY is one of 'update', 'import', 'purge' or 'manual'.</li>
+                <li>Where api_key XXX is the <i><b>API KEY / Token</b></i> defined above.</li>
+              </ul><br>
+              <h4>import & update</h4>
+              'import' and 'update' endpoint usage must include a json string
+              in the body in the format:
+              <ul>
+                <pre>[
+  {\"sam_address_id\":123,\"full_address\":\"address\",\"data\":jsonstring\"},
+  {\"sam_address_id\":123,\"full_address\":\"address\",\"data\":jsonstring\"}
+]</pre>
+                <li>The 'jsonstring' field must be a valid json object in string
+                form and can contain any data provided it represents a single
+                record and can be decoded by PHP's json_decode function.</li>
+              </ul>
+              <h4>manual</h4>
+              'manual' endpoint requires no body (any body will be ignored) but
+              must have an extended querystring in the format:
+              <ul>
+                <pre>?api_key=XXX&path=/path-on-server/file.json&limit=N&mode=ZZZ</pre>
                 <i>Required:</i>
-                  <ul><li>'path' is path on remote server (or local container) to import file.</li></ul>
+                <ul>
+                  <li>'path' is path on remote server (or local container) to
+                  import file.</li>
+                </ul>
                 <i>Optional:</i>
-                  <ul><li>'limit' process the first N records in the import file (defaults all).</li>
-                  <li>'mode' ZZZ can be either 'import' or 'update' (defaults to 'update').</li></ul>
-              <b>Note:</b>
-               <ul><li><b>UPDATE</b> updates \"full address\" and \"data\" of SAM records in the DB with
-                 \"full address\" and \"data\" from queued records with a matching \"sam_address_id\"
-                 from the json payload. New records will be created if an existing
-                 \"sam_address_id\" is not found in the DB. If there are duplicate
-                 \"sam_address_id\"s in the json payload, only the first one of the duplicate records
-                 will be processed and the rest will be skipped. No records will be deleted or marked for deletion.</li>
-               <li><b>IMPORT</b> also updates \"full address\" and \"data\" of SAM records in the DB with
-                 \"full address\" and \"data\" from queued records with a matching \"sam_address_id\"
-                 from the json payload. New records will also be created if an existing
-                 \"sam_address_id\" is not found in the DB. If there are duplicate
-                 \"sam_address_id\"s in the json payload, only the first one of the duplicate records
-                 will be processed and the rest will be skipped.  If the database contains a record (ie. a
-                 \"sam_address_id\") which is not in the import file, then after the import
-                 completes, those \"orphaned\" records will be cleaned, by removing them
-                 from the database.</li></ul>"
+                <ul>
+                  <li>'limit' process the first N records in the import file
+                  (defaults all).</li>
+                  <li>'mode' ZZZ can be either 'import' or 'update' (defaults
+                  to 'update').</li>
+                </ul>
+                <b>Note:</b>
+                <ul>
+                  <li>
+                    <b>UPDATE</b> updates 'full address' and 'data' of SAM records in the DB with
+                    'full address' and 'data' from queued records with a matching 'sam_address_id'
+                    from the json payload. New records will be created if an existing
+                    'sam_address_id' is not found in the DB. If there are duplicate
+                    'sam_address_id's in the json payload, only the first one of the duplicate records
+                    will be processed and the rest will be skipped. No records will be deleted or marked for deletion.
+                   </li>
+                  <li>
+                    <b>IMPORT</b> also updates 'full address' and 'data' of SAM records in the DB with
+                    'full address' and 'data' from queued records with a matching 'sam_address_id'
+                    from the json payload. New records will also be created if an existing
+                    'sam_address_id' is not found in the DB. If there are duplicate
+                    'sam_address_id's in the json payload, only the first one of the duplicate records
+                    will be processed and the rest will be skipped.  If the database contains a record (ie. a
+                    'sam_address_id') which is not in the import file, then after the import
+                    completes, those 'orphaned' records will be cleaned, by removing them
+                    from the database.
+                   </li>
+                </ul>
+              </ul>
+              <h4>purge</h4>
+              'purge' endpoint requires no body (any body will be ignored) but
+              must have an extended querystring in the format:
+              <ul>
+                <pre>?api_key=XXX&cutoff=[purgedate]</pre>
+                <i>Required:</i>
+                <ul>
+                  <li>
+                    purgedate is a string with the PHP function strtotime can
+                    parse, or an actual unix timestamp (int = seconds since epoch)
+                  </li>
+                </ul>
+              </ul>
+            "
             ],
         ],
         'queue_processing' => [
@@ -267,8 +390,21 @@ class MNLSettingsForm extends ConfigFormBase {
           '#title' => 'Queue Processing',
           '#collapsible' => TRUE,
           'doc' => [
-            "#markup" => "<ol><li>The REST endpoint merely loads data into a queue.</li>
-            <li>To perform and automate the update of MNL/SAM entities/nodes for use by the MNL module, 2 scheduled tasks must be created.<br><ul><li>The first task should execute the following command:<pre>drush queue:run mnl_import --items-limit=5000 &> /dev/null </pre></li><li>and the second task should execute<pre>drush queue:run mnl_update --items-limit=5000 &> /dev/null</pre></li></ul>The tasks should be scheduled to run every 5 minutes.</li></ol>
+            "#markup" => "
+              <ol>
+                <li>The REST endpoint merely loads data into a queue.</li>
+                <li>To perform and automate the update of MNL/SAM entities/nodes
+                for use by the MNL module, 2 scheduled tasks must be created.<br>
+                <ul>
+                  <li>The first task should execute the following command:
+                  <pre>drush queue:run mnl_import --items-limit=5000 && drush p:queue-work --finish &> /dev/null </pre>
+                  </li>
+                  <li>and the second task should execute
+                  <pre>drush queue:run mnl_update --items-limit=5000 && drush p:queue-work --finish &> /dev/null</pre>
+                  </li>
+                </ul>
+                The tasks should be scheduled to run every 5 minutes.</li>
+              </ol>
             <hr>
             <h4>If the scheduled tasks above are not created then the website will not be updated until the commands above are run manually.</h4>            "
           ],
@@ -325,7 +461,7 @@ class MNLSettingsForm extends ConfigFormBase {
    *
    * @return string
    */
-  private function _buildStatus(int $status) {
+  private function _buildStatus($status = -1) {
     $css = "padding:10px;font-weight:bold;display: inline;";
     switch ($status) {
       case MnlUtilities::MNL_IMPORT_IDLE:
