3776f48297ab9c988f434d9f35d0f4e24e406181
David Upton <davidrkupton@gmail.com>
2022-08-31 17:27:59 -0400

    Better salesforce tracking. DIG-775

--- docroot/modules/custom/bos_components/modules/bos_email/src/Templates/MetrolistListingNotification.php
+++ docroot/modules/custom/bos_components/modules/bos_email/src/Templates/MetrolistListingNotification.php
@@ -40,7 +40,7 @@ Contact Email: ${vars["contact_email"]}\n
 Contact Phone: ${vars["contact_phone"]}\n\n
 Property Name: ${vars["property_name"]}\n
 Property Address: ${vars["street_address"]}, ${vars["city"]}, ${vars["zip_code"]}\n
-View Pending Development Units: https://boston-dnd.lightning.force.com/lightning/o/Development_Unit__c/list?filterName=${vars["developmentsfid"]}\n\n
+View Pending Development: https://boston-dnd.lightning.force.com/lightning/r/Development__c/${vars["developmentsfid"]}/view${vars["developmentsfid"]}\n\n
 -------------------------------- \n
 This message was sent using the Metrolist Listing form on Boston.gov " . urldecode($emailFields['url']) . ".\n\n
 -------------------------------- \n
@@ -65,21 +65,28 @@ This message was sent using the Metrolist Listing form on Boston.gov " . urldeco
 
     $weblink = "";
     if (!empty($vars["website_link"])) {
-      $weblink = "<p class='txt'><a href='${vars["website_link"]}'>Property Link</a></p>\n";
+      $weblink = "<p class='txt'><a href='${vars["website_link"]}'>Property Link</a></p>";
+    }
+    $contact = "${vars["contact_name"]}";
+    if (!empty($vars["contactsfid"])) {
+      $contact = "<a href='https://boston-dnd.lightning.force.com/lightning/r/Contact/${vars["contactsfid"]}/view'>${vars["contact_name"]}</a>";
+    }
+    $development = $vars["property_name"];
+    if (!empty($vars["developmentsfid"])) {
+      $contact = "<a href='https://boston-dnd.lightning.force.com/lightning/r/Development__c/${vars["developmentsfid"]}/view'>${vars["property_name"]}</a>";
     }
 
     $html = "
 <img class='ml-icon' height='34' src='https://assets.boston.gov/icons/metrolist/metrolist-logo_email.png' />\n
 <p class='txt'>A new listing has been submitted to Metrolist:</p>\n
 <p class='txt'><span class='txt-b'>Submitted on:</span> ${vars["completed"]}</p>\n
-<p class='txt'><span class='txt-b'>Submitted By:</span> ${vars["contact_name"]}</p>\n
+<p class='txt'><span class='txt-b'>Submitted By:</span> ${contact}</p>\n
 <p class='txt'><span class='txt-b'>Listing Contact Company:</span> ${vars["contact_company"]}</p>\n
 <p class='txt'><span class='txt-b'>Contact Email:</span> ${vars["contact_email"]}</p>\n
 <p class='txt'><span class='txt-b'>Contact Phone:</span> ${vars["contact_phone"]}</p>\n
-<p class='txt'><span class='txt-b'>Property Name:</span> ${vars["property_name"]}</p>\n
+<p class='txt'><span class='txt-b'>Property Name:</span> ${development}</p>\n
 <p class='txt'><span class='txt-b'>Property Address:</span> ${vars["street_address"]}, ${vars["city"]}, ${vars["zip_code"]}</p>\n
-${weblink}
-<p class='txt'><a href='https://boston-dnd.lightning.force.com/lightning/o/Development_Unit__c/list?filterName=${vars["developmentsfid"]}'>View Pending Development Units</a></p>\n
+${weblink}\n
 <hr>
 <p class='txt'>This message was sent using the <a href='${emailFields['url']}'>Metrolist Listing Form</a> on Boston.gov.</p>\n\n
 <hr>\n
@@ -124,16 +131,17 @@ ${weblink}
       "sid" => $request->get("sid",""),
       "serial" => $request->get("serial",""),
       "property_name" => $request->get("property_name",""),
-      "completed" => $request->get("completed",""),
+      "completed" => gmdate("Y-m-d H:i", $request->get("completed","")),
       "contact_name" => $request->get("contact_name",""),
       "contact_company" => $request->get("contact_company",""),
       "contact_email" => $request->get("contact_email",""),
       "contact_phone" => $request->get("contact_phone",""),
       "street_address" => $request->get("street_address",""),
-      "city" => $request->get("city",""),
+      "city" => $request->get("neighborhood",""),
       "zip_code" => $request->get("zip_code",""),
       "website_link" => $request->get("website_link",""),
       "developmentsfid" => $request->get("developmentsfid",""),
+      "contactsfid" => $request->get("contactsfid",""),
     ];
     return $output;
   }
