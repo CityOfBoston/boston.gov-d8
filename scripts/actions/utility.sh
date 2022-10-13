7dc8d0d9926efeecb8c1eacc95ce8e45154ef62f
David Upton <davidrkupton@gmail.com>
2022-10-13 14:14:14 -0400

    Fixes config import:siteuuid etc. DIG-1078

--- scripts/actions/utility.sh
+++ scripts/actions/utility.sh
@@ -88,3 +88,37 @@ function printout () {
         printf "\n"
     fi
 }
+
+# Synchronize the Site UUID in Database with UUID in system.site.yml
+# Argument 1 is the path to the config files, and argument 2 directs whether the DB or config file is updated.
+function verifySiteUUID() {
+  # Each Drupal site has a unique site UUID.
+  # If we have exported configs from an existing site, and try to import them into a new (or different) site, then
+  # Drupal recognizes this and prevents the entire import.
+  # Since the configs saved in the repo are from a different site than the one we have just created, the UUID in
+  # the configs wont match the UUID in the database.  To continue, we need to update the UUID of the new site to
+  # be the same as that in the </config/default/system.site.yml> file.
+
+  configPath="${1}"
+  if [[ -s ${configPath}/system.site.yml ]]; then
+    # Fetch site UUID from the configs in the (newly made) database.
+    db_uuid=$(${drush_cmd} @self config:get "system.site" "uuid" | grep -Eo "\s[0-9a-h\-]*")
+    # Fetch the site UUID from the configuration file.
+    yml_uuid=$(cat ${configPath}/system.site.yml | grep "uuid:" | grep -Eo "\s[0-9a-h\-]*")
+
+    if [[ "${db_uuid}" != "${yml_uuid}" ]]; then
+      # The config UUID is different to the UUID in the database.
+      printout "NOTICE" "UUID in database needs to be updated to ${yml_uuid}."
+
+      # Change the databases UUID to match the config files UUID.
+      ${drush_cmd} @self config:set "system.site" "uuid" ${yml_uuid} -y &>/dev/null
+
+      if [[ $? -eq 0 ]]; then
+        printout "SUCCESS" "UUID in database is updated."
+      else
+        printout "WARNING" "Updating UUID in database failed."
+        return 1
+      fi
+    fi
+  fi
+}
