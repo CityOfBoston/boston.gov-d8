f6bdfebe3382402c89573af2ff337df7b2c2d4d4
Fritz Medine <fritz.medine@milespartnership.com>
2023-01-30 09:42:40 -0500

    removed import select

--- docroot/modules/custom/bos_components/modules/bos_web_app/apps/metrolist/src/components/Selectbox/index.js
+++ docroot/modules/custom/bos_components/modules/bos_web_app/apps/metrolist/src/components/Selectbox/index.js
@@ -1,13 +1,3 @@
 import React from 'react'
-import Select from 'react-select'
 
-const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' }
-]
-
-const Selectbox = () => (
-  <Select options={options} />
-)
 export default Selectbox;
