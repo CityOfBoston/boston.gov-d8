177039f26ae143034b91c9a0080300dac66271a6
David Upton <davidrkupton@gmail.com>
2024-02-23 15:37:17 -0500

    DIG-3968 Adds entity event dispatcher.

---
+++ docroot/modules/custom/bos_core/src/BosCoreEntityEventType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Drupal\bos_core;
+
+use Drupal\entity_events\EntityEventType;
+
+class BosCoreEntityEventType extends EntityEventType {
+
+  const PRESAVE = "event.presave";
+
+}
