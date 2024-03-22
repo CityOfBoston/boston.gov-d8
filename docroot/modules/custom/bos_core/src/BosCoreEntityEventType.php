39b87cf8e365124ee8249928635baa287705a5eb
David Upton <davidrkupton@gmail.com>
2024-03-22 15:19:40 -0400

    DIG-3968 Make dependency conditional in bos_core..

--- docroot/modules/custom/bos_core/src/BosCoreEntityEventType.php
+++ docroot/modules/custom/bos_core/src/BosCoreEntityEventType.php
@@ -4,7 +4,10 @@ namespace Drupal\bos_core;
 
 use Drupal\entity_events\EntityEventType;
 
-class BosCoreEntityEventType extends EntityEventType {
-  const LOAD = "event.load";
+if (class_exists(EntityEventType::class)) {
 
+  class BosCoreEntityEventType extends EntityEventType {
+    const LOAD = "event.load";
+
+  }
 }
