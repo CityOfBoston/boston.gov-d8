3b702f5d7533816936f6bcd752c965d55177a919
David Upton <davidrkupton@gmail.com>
2024-05-29 23:49:39 -0400

    DIG-4438 Updates Backend form layout

--- docroot/modules/custom/bos_components/modules/bos_charts/js/bos_charts_vega.js
+++ docroot/modules/custom/bos_components/modules/bos_charts/js/bos_charts_vega.js
@@ -6,6 +6,18 @@ jQuery(document).ready(function ($) {
   for(const chartid in drupalSettings.cob.charts) {
     var chart = drupalSettings.cob.charts[chartid];
     var VegaLiteSpec = JSON.parse(chart.chartobj);
+
+    // Replace dataURL in object if provided.
+    if (typeof chart.dataUrl !== "undefined") {
+      VegaLiteSpec.data.url = chart.dataUrl;
+    }
+
+    // Ensure the background is set to "none" for theming.
+    if (typeof VegaLiteSpec.config === "undefined") {
+      VegaLiteSpec.config = {};
+    }
+    VegaLiteSpec.config.background = "none";
+
     vegaEmbed('#' + chart.chartid , VegaLiteSpec, opt)
       .then(function (result) {
       })
