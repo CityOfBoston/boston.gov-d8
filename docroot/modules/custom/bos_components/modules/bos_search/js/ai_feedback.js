752232e2b1b46bdf25bcd356bf7a7d259e96f2fc
David Upton <davidrkupton@gmail.com>
2024-09-12 19:29:14 -0400

    DIG-5017 Adds feedback.

--- docroot/modules/custom/bos_components/modules/bos_search/js/ai_feedback.js
+++ docroot/modules/custom/bos_components/modules/bos_search/js/ai_feedback.js
@@ -5,19 +5,22 @@
         function (element) {
           $(document).on("ajaxComplete", function (event, xhr, settings) {
             if (xhr.statusText.toString() === 'success') {
-              var dialog = $('.feedback-dialog');
-              if (dialog.length > 0) {
-                var more = dialog.find('textarea[name=tell_us_more]');
-                more.on("keyup", function(element){
-                  var textbox = $(element.target).val();
-                  var count = parseInt(textbox.length);
-                  if (!count) {
-                    dialog.find('.text-count-message').text('200 characters allowed');
-                  }
-                  else {
-                    dialog.find('.text-count-message').text((200 - count) + ' characters remaining');
-                  }
-                });
+              var thisdialog = $('.feedback-dialog');
+              if (settings.url.toString().startsWith("/form/ai-search-feedback") && thisdialog.length > 0) {
+                if (thisdialog.find(".text-count-message").length > 0) {
+                  var more = thisdialog.find('textarea[name=tell_us_more]');
+                  more.on("keyup", function(element){textarea_counter(element.target, thisdialog);})
+                }
+                else {
+                  var form = $(".feedback-dialog .ui-dialog-content");
+                  var message = form.text().trim("\n");
+                  $(".aienabledsearchform .ai-feedback-confirm").last().text(message).show();
+                  $(".aienabledsearchform .ai-feedback-buttons").last().hide();
+                  var searchform = $('.aienabledsearchform');
+                  var results = $('.search-results-wrapper').last();
+                  fb_move_div_to_top(searchform, results);
+                  form.dialog("close");
+                }
               }
             }
           });
@@ -25,4 +28,30 @@
       );
     }
   };
+
+  var textarea_counter = function (element, thisdialog) {
+    var textbox = $(element).val();
+    var count = parseInt(textbox.length);
+    if (!count) {
+      thisdialog.find('.text-count-message').text('200 characters allowed');
+    }
+    else {
+      thisdialog.find('.text-count-message').text((200 - count) + ' characters remaining');
+    }
+  };
+
+  var fb_move_div_to_top = function(searchform, div) {
+    if ($(".search-response-wrapper").length) {
+      var offsetHeight = ((div.offset().top) - (searchform.offset().top) - window.height);
+      var scroll_layer = $("html, body");
+      if (searchform.hasClass("aisearch-modal-form")) {
+        scroll_layer = searchform;
+        offsetHeight = ((div.offset().top) - (searchform.offset().top) - window.height );
+      }
+      scroll_layer.animate({
+        scrollTop: offsetHeight,
+      }, 'fast');
+    }
+  }
+
 })(jQuery, Drupal, once);
