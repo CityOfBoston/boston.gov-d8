9eaca026f4926d99d705d98299ae4442c1737753
Kane Xu <xu.hong@northeastern.edu>
2024-09-25 16:41:33 -0400

    Updated Search Bar Component Styling, Metrolist Drupal Config

--- docroot/modules/custom/bos_components/modules/bos_web_app/apps/metrolist/src/components/SearchBar/index.js
+++ docroot/modules/custom/bos_components/modules/bos_web_app/apps/metrolist/src/components/SearchBar/index.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Stack from '@components/Stack';
-import { formatIncome } from '@util/strings';
 import { getGlobalThis } from '@util/objects';
 
 const globalThis = getGlobalThis();
